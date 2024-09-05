630c10173779655bb7337d82394061ca13a8115b
dudxo <father8475@naver.com>
2024-09-05 20:07:02 +0900

    [feat] : 일반 회원 Entity 추가

---
+++ src/main/java/org/c4marathon/assignment/user/domain/User.java
@@ -0,0 +1,38 @@
+package org.c4marathon.assignment.user.domain;
+
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import org.c4marathon.assignment.common.domain.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "users_id")
+	private Long id;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Enumerated(STRING)
+	@Column(name = "role", nullable = false)
+	private UserRole role;
+}
