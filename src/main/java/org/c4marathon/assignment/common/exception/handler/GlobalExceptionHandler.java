6b68cdaa44b8a0a5a41b8cab366bc51d4e335307
dudxo <father8475@naver.com>
2024-09-05 21:07:20 +0900

    [feat] : 전역 예외처리 관련 클래스 추가

---
+++ src/main/java/org/c4marathon/assignment/common/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package org.c4marathon.assignment.common.exception.handler;
+
+import org.c4marathon.assignment.common.exception.runtime.BaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BaseException.class)
+	public ExceptionResponse customException(BaseException baseException) {
+		return new ExceptionResponse(baseException.getCode(), baseException.getMessage());
+	}
+
+	private record ExceptionResponse(
+		String errorCode,
+		String message
+	) {
+	}
+}
---
+++ src/main/java/org/c4marathon/assignment/common/exception/runtime/ErrorCode.java
@@ -0,0 +1,8 @@
+package org.c4marathon.assignment.common.exception.runtime;
+
+public interface ErrorCode {
+
+	String getMessage();
+
+	String getCode();
+}
