6bb5a33e50d44c72fbfe02dd1569bbc37211906b
dudxo <father8475@naver.com>
2024-09-05 21:07:42 +0900

    [feat] : 회원가입 API 추가

---
+++ src/main/java/org/c4marathon/assignment/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package org.c4marathon.assignment.user.controller;
+
+import org.c4marathon.assignment.user.dto.request.JoinRequestDto;
+import org.c4marathon.assignment.user.dto.response.JoinResponseDto;
+import org.c4marathon.assignment.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/api/user/join")
+	public ResponseEntity<JoinResponseDto> join(
+		@Valid
+		@RequestBody JoinRequestDto joinRequestDto
+	) {
+		JoinResponseDto joinResponseDto = userService.join(joinRequestDto);
+		return ResponseEntity.ok(joinResponseDto);
+	}
+}
