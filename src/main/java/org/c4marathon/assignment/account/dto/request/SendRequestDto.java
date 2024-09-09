c3f5169c50eb3579bccd2edfffd7f211cd093c89
dudxo <father8475@naver.com>
2024-09-10 00:45:55 +0900

    [fix] : Long vlidation 어노테이션 타입오류 해결

--- src/main/java/org/c4marathon/assignment/account/dto/request/SendRequestDto.java
+++ src/main/java/org/c4marathon/assignment/account/dto/request/SendRequestDto.java
@@ -2,19 +2,18 @@ package org.c4marathon.assignment.account.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SendRequestDto(
-
-	@NotBlank(message = "계좌번호는 필수 입력 항목입니다.")
+	@NotNull(message = "계좌번호는 필수 입력 항목입니다.")
 	Long toAccountId,
 	@NotBlank(message = "계좌 타입는 필수 입력 항목입니다.")
 	String toAccountType,
 	@Min(value = 0, message = "송금금액은 0원 이상이어야 합니다.")
 	int sendToMoney,
-	@NotBlank(message = "계좌번호는 필수 입력 항목입니다.")
+	@NotNull(message = "계좌번호는 필수 입력 항목입니다.")
 	Long fromAccountId,
 	@NotBlank(message = "계좌 타입는 필수 입력 항목입니다.")
-
 	String fromAccountType
 
 ) {
