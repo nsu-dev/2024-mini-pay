9708256cc9fbca7a0038257408ea6db6bebcc313
dudxo <father8475@naver.com>
2024-09-09 20:12:41 +0900

    [feat] : 유저 에러코드 추가

--- src/main/java/org/c4marathon/assignment/user/exception/UserErrorCode.java
+++ src/main/java/org/c4marathon/assignment/user/exception/UserErrorCode.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-	DUPLICATED_EMAIL("중복된 이메일 회원이 존재합니다.", "USER_001");
+	DUPLICATED_EMAIL("중복된 이메일 회원이 존재합니다.", "USER_001"),
+	NOT_FOUND_USER("해당 사용자를 찾을 수 없습니다.", "USER_002"),
+	NOT_MATCH_PASSWORD("비밀번호가 일치하지 않습니다.", "USER_003");
 
 	private final String message;
 	private final String code;
