c1b18c4eaeb5bf88d67769f6adb41eed74b5599c
dudxo <father8475@naver.com>
2024-09-09 19:11:47 +0900

    [feat] : User <-> Account 일대다 연관관계 추가

--- src/main/java/org/c4marathon/assignment/user/domain/User.java
+++ src/main/java/org/c4marathon/assignment/user/domain/User.java
@@ -4,13 +4,19 @@ import static jakarta.persistence.EnumType.*;
 import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.c4marathon.assignment.account.domain.Account;
 import org.c4marathon.assignment.common.domain.BaseTimeEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
@@ -39,6 +45,9 @@ public class User extends BaseTimeEntity {
 	@Enumerated(STRING)
 	@Column(name = "role", nullable = false)
 	private UserRole role;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+	private List<Account> accountList = new ArrayList<>();
 
 	@Builder
 	private User(String email, String password, String name, UserRole role) {
