0d450ac5f93be6e8e198ca90c9f057b50b3499b9
조아빈 <whdkqls122@naver.com>
2024-09-10 03:43:06 +0900

    [fix] : Account 엔티티 조인조건 수정

--- src/main/java/org/c4marathon/assignment/domain/account/entity/Account.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/Account.java
@@ -2,14 +2,19 @@ package org.c4marathon.assignment.domain.account.entity;
 
 import java.time.LocalDateTime;
 
+import org.c4marathon.assignment.domain.user.entity.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -31,4 +36,21 @@ public class Account {
 	@Enumerated(EnumType.STRING)
 	@Column(name = "accountStatus", nullable = false)
 	private AccountStatus accountStatus;
+	@Column(name = "dailyChargeLimit", nullable = false)
+	private int dailyChargeLimit;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	@Builder
+	private Account(String accountNum, AccountRole accountRole, LocalDateTime registeredAt, Long accountBalance,
+		AccountStatus accountStatus, int dailyChargeLimit, User user) {
+		this.accountNum = accountNum;
+		this.accountRole = accountRole;
+		this.registeredAt = registeredAt;
+		this.accountBalance = accountBalance;
+		this.accountStatus = accountStatus;
+		this.dailyChargeLimit = dailyChargeLimit;
+		this.user = user;
+	}
 }
