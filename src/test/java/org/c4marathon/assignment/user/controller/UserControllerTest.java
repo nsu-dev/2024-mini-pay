1e20b0705721ef4999e97e1fc40cabcb167ae615
dudxo <father8475@naver.com>
2024-09-09 20:47:38 +0900

    [test] : 로그인 API 통합테스트 추가

--- src/test/java/org/c4marathon/assignment/user/controller/UserControllerTest.java
+++ src/test/java/org/c4marathon/assignment/user/controller/UserControllerTest.java
@@ -1,11 +1,14 @@
 package org.c4marathon.assignment.user.controller;
 
+import static org.c4marathon.assignment.user.domain.UserRole.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.c4marathon.assignment.account.repository.AccountRepository;
+import org.c4marathon.assignment.user.domain.User;
 import org.c4marathon.assignment.user.dto.request.JoinRequestDto;
+import org.c4marathon.assignment.user.dto.request.LoginRequestDto;
 import org.c4marathon.assignment.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -33,6 +37,9 @@ class UserControllerTest {
 	@Autowired
 	private AccountRepository accountRepository;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@AfterEach
 	void tearDown() {
 		userRepository.deleteAll();
@@ -52,5 +59,27 @@ class UserControllerTest {
 			)
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.email").value("join@mini.com"));
+	}
+
+	@DisplayName("[로그인 테스트]")
+	@Test
+	void loginApi() throws Exception {
+		// given
+		final String password = "mini1234";
+		User user = User.builder()
+			.email("abc@mini.com")
+			.name("김미니")
+			.password(passwordEncoder.encode(password))
+			.role(USER)
+			.build();
+		userRepository.save(user);
+		LoginRequestDto request = new LoginRequestDto(user.getEmail(), password);
+
+		// when		// then
+		mockMvc.perform(post("/api/user/login")
+				.content(objectMapper.writeValueAsString(request))
+				.contentType(APPLICATION_JSON)
+			)
+			.andExpect(status().isOk());
 	}
 }
