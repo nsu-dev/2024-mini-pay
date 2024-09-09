baccced164db34ae0aa13b68a936b48b6205e6a2
dudxo <father8475@naver.com>
2024-09-09 19:11:18 +0900

    [feat] : 계좌 Account 엔티티 추가

---
+++ src/main/java/org/c4marathon/assignment/account/domain/Account.java
@@ -0,0 +1,54 @@
+package org.c4marathon.assignment.account.domain;
+
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import org.c4marathon.assignment.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "account")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "account_id")
+	private Long id;
+
+	@Enumerated(STRING)
+	@Column(name = "account_type", nullable = false)
+	private AccountType type;
+
+	@Column(name = "amount", nullable = false)
+	private int amount;
+
+	@Column(name = "limit_amount", nullable = false)
+	private int limitAmount;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "users_id")
+	private User user;
+
+	@Builder
+	private Account(AccountType type, int amount, int limitAmount, User user) {
+		this.type = type;
+		this.amount = amount;
+		this.limitAmount = limitAmount;
+		this.user = user;
+	}
+}
