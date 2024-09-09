3cf7b79f8ad69d88794c9f46272e3635479c471c
dudxo <father8475@naver.com>
2024-09-09 20:12:29 +0900

    [feat] : 로그인 비즈니스 로직 및 회원가입 비밀번호 인코딩 추가

--- src/main/java/org/c4marathon/assignment/user/service/UserService.java
+++ src/main/java/org/c4marathon/assignment/user/service/UserService.java
@@ -1,14 +1,20 @@
 package org.c4marathon.assignment.user.service;
 
+import java.util.Date;
+
 import org.c4marathon.assignment.common.exception.runtime.BaseException;
+import org.c4marathon.assignment.common.jwt.JwtProvider;
 import org.c4marathon.assignment.event.account.AccountEvent;
 import org.c4marathon.assignment.user.domain.User;
 import org.c4marathon.assignment.user.dto.UserMapper;
 import org.c4marathon.assignment.user.dto.request.JoinRequestDto;
+import org.c4marathon.assignment.user.dto.request.LoginRequestDto;
 import org.c4marathon.assignment.user.dto.response.JoinResponseDto;
+import org.c4marathon.assignment.user.dto.response.LoginResponseDto;
 import org.c4marathon.assignment.user.exception.UserErrorCode;
 import org.c4marathon.assignment.user.repository.UserRepository;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +26,8 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final ApplicationEventPublisher eventPublisher;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtProvider jwtProvider;
 
 	@Transactional
 	public JoinResponseDto join(JoinRequestDto joinRequestDto) {
@@ -28,7 +36,7 @@ public class UserService {
 			throw new BaseException(UserErrorCode.DUPLICATED_EMAIL);
 		}
 
-		User joinUser = UserMapper.toUserFromJoinRequestDto(joinRequestDto);
+		User joinUser = UserMapper.toUserFromJoinRequestDto(joinRequestDto, passwordEncoder);
 		User savedUser = userRepository.save(joinUser);
 
 		eventPublisher.publishEvent(new AccountEvent(savedUser));
@@ -38,5 +46,18 @@ public class UserService {
 
 	private boolean checkDuplicatedEmail(String email) {
 		return userRepository.existsByEmail(email);
+	}
+
+	public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+		User findUser = userRepository.findByEmail(loginRequestDto.email())
+			.orElseThrow(() -> new BaseException(UserErrorCode.NOT_FOUND_USER));
+
+		if (!passwordEncoder.matches(loginRequestDto.password(), findUser.getPassword())) {
+			new BaseException(UserErrorCode.NOT_MATCH_PASSWORD);
+		}
+
+		String token = jwtProvider.createToken(findUser, new Date());
+
+		return UserMapper.toLoginResponseDto(token);
 	}
 }
