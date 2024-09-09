9e56a1df9cd101139af71b57d92aa334c9480181
dudxo <father8475@naver.com>
2024-09-09 20:13:01 +0900

    [feat] : 이메일로 유저 찾는 쿼리메서드 추가

--- src/main/java/org/c4marathon/assignment/user/repository/UserRepository.java
+++ src/main/java/org/c4marathon/assignment/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.user.repository;
 
+import java.util.Optional;
+
 import org.c4marathon.assignment.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	boolean existsByEmail(String email);
+
+	Optional<User> findByEmail(String Email);
 }
