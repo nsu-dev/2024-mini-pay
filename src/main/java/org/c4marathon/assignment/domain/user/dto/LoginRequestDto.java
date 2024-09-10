08ce1066c8bacde8fa43c0f770303e19ed66eb28
조아빈 <whdkqls122@naver.com>
2024-09-10 15:51:48 +0900

    [feat] : 로그인 요청 dto 생성

---
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package org.c4marathon.assignment.domain.user.dto;
+
+public class LoginRequsetDto {
+}
--- src/main/java/org/c4marathon/assignment/domain/user/dto/UserDto.java
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/UserDto.java
@@ -2,28 +2,25 @@ package org.c4marathon.assignment.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
-@Getter
 public class UserDto {
-    @NotBlank
-    private String userPhone;
-    @NotBlank
-    private String password;
+	@NotBlank
+	private String userPhone;
+	@NotBlank
+	private String password;
 
-    @NotBlank
-    private String userName;
+	@NotBlank
+	private String userName;
 
-    @NotBlank
-    private String userBirth;
+	@NotBlank
+	private String userBirth;
 
-
-
-    public UserDto(@NotBlank String userPhone, @NotBlank String userName, @NotBlank String userBirth, @NotBlank String password) {
-        this.userPhone = userPhone;
-        this.userName = userName;
-        this.userBirth = userBirth;
-        this.password = password;
-    }
+	public UserDto(@NotBlank String userPhone, @NotBlank String userName, @NotBlank String userBirth,
+		@NotBlank String password) {
+		this.userPhone = userPhone;
+		this.userName = userName;
+		this.userBirth = userBirth;
+		this.password = password;
+	}
 }
