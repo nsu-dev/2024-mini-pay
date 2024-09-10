47d9183313b2ed78d2d1c706f381d761c704581f
조아빈 <whdkqls122@naver.com>
2024-09-10 15:52:10 +0900

    [feat] : 로그인 요청 dto 생성

--- src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
@@ -1,4 +1,10 @@
 package org.c4marathon.assignment.domain.user.dto;
 
-public class LoginRequsetDto {
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequestDto {
+	@NotBlank
+	private String userPhone;
+	@NotBlank
+	private String userPassword;
 }
