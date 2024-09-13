794da32ab04c55023d5d01c2ee7e3a99cc123a07
dudxo <father8475@naver.com>
2024-09-13 18:39:09 +0900

    [feat] : 계좌 충전 관련 에러코드 추가

--- src/main/java/org/c4marathon/assignment/account/exception/AccountErrorCode.java
+++ src/main/java/org/c4marathon/assignment/account/exception/AccountErrorCode.java
@@ -13,7 +13,8 @@ public enum AccountErrorCode implements ErrorCode {
 	NOT_FOUND_ACCOUNT("계좌를 찾을 수 없습니다.", "ACCOUNT_002"),
 	NOT_ENOUGH_AMOUNT("계좌 금액이 충분하지 않습니다.", "ACCOUNT_003"),
 	NOT_AUTHORIZED_ACCOUNT("계좌 인출 권환이 없습니다.", "ACCOUNT_004"),
-	NOT_ENOUGH_CHARGE_AMOUNT("충전 한도를 초과했습니다.", "ACCOUNT_005");
+	NOT_ENOUGH_CHARGE_AMOUNT("충전 한도를 초과했습니다.", "ACCOUNT_005"),
+	NOT_ACCESS_CHARGE("계좌 금액 충전에 접근할 수 없는 계좌입니다.", "ACCOUNT_006");
 
 	private final String message;
 	private final String code;
