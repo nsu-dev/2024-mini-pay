c98a7553dd7ebf7c53693ed9d8ec8d5fb6c4128e
조아빈 <whdkqls122@naver.com>
2024-09-15 18:35:39 +0900

    [feat] : 예외발생 시 반환하는 Dto 생성

--- src/main/java/org/c4marathon/assignment/domain/account/dto/ErrDto.java
+++ src/main/java/org/c4marathon/assignment/domain/account/dto/ErrDto.java
@@ -1,2 +1,6 @@
-package org.c4marathon.assignment.domain.account.dto;public record ErrDto() {
+package org.c4marathon.assignment.domain.account.dto;
+
+import org.springframework.http.HttpStatus;
+
+public record ErrDto(int responseStatus, String responseErrMsg, HttpStatus httpStatus) {
 }
