3619d0de86e693d18c57591d1f57c7e1b6cb1bea
조아빈 <whdkqls122@naver.com>
2024-09-15 22:08:11 +0900

    [fix] ExceptionHandler 적용에 따른 UserService  수정

--- src/main/java/org/c4marathon/assignment/domain/user/repository/UserRepository.java
+++ src/main/java/org/c4marathon/assignment/domain/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package org.c4marathon.assignment.domain.user.repository;
 
+import java.util.Optional;
+
 import org.c4marathon.assignment.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByUserPhone(String userPhone);
 
-	User findByUserPhone(String userPhone);
+	Optional<User> findByUserPhone(String userPhone);
 }
--- src/main/java/org/c4marathon/assignment/domain/user/service/UserService.java
+++ src/main/java/org/c4marathon/assignment/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.domain.user.service;
 
+import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
+
 import org.c4marathon.assignment.domain.account.entity.ScheduleCreateEvent;
 import org.c4marathon.assignment.domain.user.dto.JoinResponseDto;
 import org.c4marathon.assignment.domain.user.dto.LoginRequestDto;
@@ -9,12 +11,11 @@ import org.c4marathon.assignment.domain.user.dto.UserMapper;
 import org.c4marathon.assignment.domain.user.entity.JoinResponseMsg;
 import org.c4marathon.assignment.domain.user.entity.LoginResponseMsg;
 import org.c4marathon.assignment.domain.user.entity.User;
+import org.c4marathon.assignment.domain.user.exception.UserException;
 import org.c4marathon.assignment.domain.user.repository.UserRepository;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpClientErrorException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -33,35 +34,27 @@ public class UserService {
 			userRepository.save(user);
 			eventPublisher.publishEvent(new ScheduleCreateEvent(user));
 
-            return JoinResponseDto.builder()
+			return JoinResponseDto.builder()
 				.responseMsg(JoinResponseMsg.SUCCESS.getResponseMsg())
 				.build();
 		} else {
-			throw new HttpClientErrorException(HttpStatusCode.valueOf(400));
+			throw new UserException(USER_DUPLICATED_FAIL);
 		}
 	}
 
 	//로그인
 	public LoginResponseDto login(LoginRequestDto loginRequestDto) {
-		if (userRepository.existsByUserPhone(loginRequestDto.getUserPhone())) {
-			User user = userRepository.findByUserPhone(loginRequestDto.getUserPhone());
-			if (loginRequestDto.getUserPassword().equals(user.getUserPassword())) {
-				return LoginResponseDto.builder()
-					.responseMsg(LoginResponseMsg.SUCCESS.getResponseMsg())
-					.build();
-			} else {
-				return LoginResponseDto.builder()
-					.responseMsg(LoginResponseMsg.FAIL.getResponseMsg())
-					.build();
-			}
+		User user = userRepository.findByUserPhone(loginRequestDto.getUserPhone())
+			.orElseThrow(() -> new UserException(USER_NOT_FOUND));
+		if (!loginRequestDto.getUserPassword().equals(user.getUserPassword())) {
+			throw new UserException(USER_LOGIN_FAIL);
 		}
-		throw new HttpClientErrorException(HttpStatusCode.valueOf(400));
+		return LoginResponseDto.builder()
+			.responseMsg(LoginResponseMsg.SUCCESS.getResponseMsg())
+			.build();
 	}
 
 	private boolean duplicatedUser(String userPhone) {
-		if (userRepository.existsByUserPhone(userPhone)) {
-			return false;
-		}
-		return true;
+		return !userRepository.existsByUserPhone(userPhone);
 	}
 }
