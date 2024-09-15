845e60195637fcc4987978ee0871bfbc47bacd47
조아빈 <whdkqls122@naver.com>
2024-09-15 22:50:44 +0900

    [feat] : 비밀번호 암호화 로직 추가

--- src/main/java/org/c4marathon/assignment/domain/user/dto/UserMapper.java
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/UserMapper.java
@@ -1,16 +1,19 @@
 package org.c4marathon.assignment.domain.user.dto;
 
 import org.c4marathon.assignment.domain.user.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
-	public static User toUser(UserDto userDto) {
+	PasswordEncoder passwordEncoder;
+
+	public static User toUser(UserDto userDto, PasswordEncoder passwordEncoder) {
 		return User.builder()
 			.userPhone(userDto.getUserPhone())
-			.userPassword(userDto.getUserPassword())
+			.userPassword(passwordEncoder.encode(userDto.getUserPassword()))
 			.userName(userDto.getUserName())
 			.userBirth(userDto.getUserBirth())
 			.build();
--- src/main/java/org/c4marathon/assignment/domain/user/service/UserService.java
+++ src/main/java/org/c4marathon/assignment/domain/user/service/UserService.java
@@ -14,6 +14,7 @@ import org.c4marathon.assignment.domain.user.entity.User;
 import org.c4marathon.assignment.domain.user.exception.UserException;
 import org.c4marathon.assignment.domain.user.repository.UserRepository;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 	private final UserRepository userRepository;
 	private final ApplicationEventPublisher eventPublisher;
+	private final PasswordEncoder passwordEncoder;
 
 	//회원가입
 	@Transactional
@@ -33,7 +35,7 @@ public class UserService {
 			throw new UserException(USER_DUPLICATED_FAIL);
 
 		}
-		User user = UserMapper.toUser(userDto);
+		User user = UserMapper.toUser(userDto, passwordEncoder);
 		userRepository.save(user);
 		eventPublisher.publishEvent(new ScheduleCreateEvent(user));
 
