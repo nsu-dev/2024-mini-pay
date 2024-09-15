2e4bf8291179fee89474e3165ad4cf232d2549c4
조아빈 <whdkqls122@naver.com>
2024-09-15 21:25:49 +0900

    [fix] ExceptionHandler 적용에 따른 UserController 수정

--- src/main/java/org/c4marathon/assignment/domain/user/controller/UserController.java
+++ src/main/java/org/c4marathon/assignment/domain/user/controller/UserController.java
@@ -1,18 +1,18 @@
 package org.c4marathon.assignment.domain.user.controller;
 
+import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
+
 import org.c4marathon.assignment.domain.user.dto.JoinResponseDto;
 import org.c4marathon.assignment.domain.user.dto.LoginRequestDto;
 import org.c4marathon.assignment.domain.user.dto.LoginResponseDto;
 import org.c4marathon.assignment.domain.user.dto.UserDto;
-import org.c4marathon.assignment.domain.user.entity.JoinResponseMsg;
-import org.c4marathon.assignment.domain.user.entity.LoginResponseMsg;
+import org.c4marathon.assignment.domain.user.exception.UserException;
 import org.c4marathon.assignment.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,35 +27,19 @@ public class UserController {
 	public ResponseEntity<JoinResponseDto> userJoin(@Valid @RequestBody UserDto userDto) {
 		try {
 			JoinResponseDto joinResponseDto = userService.join(userDto);
-
-			if (joinResponseDto.responseMsg().equals(JoinResponseMsg.SUCCESS.getResponseMsg())) {
-				return ResponseEntity.ok().body(joinResponseDto);
-			} else {
-				return ResponseEntity.badRequest().body(joinResponseDto);
-			}
-		} catch (HttpClientErrorException e) {
-			JoinResponseDto joinResponseDto = JoinResponseDto.builder()
-				.responseMsg(JoinResponseMsg.DUPLICATIEDFAIL.getResponseMsg())
-				.build();
-			return ResponseEntity.status(e.getStatusCode()).body(joinResponseDto);
+			return ResponseEntity.ok().body(joinResponseDto);
+		} catch (RuntimeException e) {
+			throw new UserException(USER_SERVER_ERROR);
 		}
-
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponseDto> userLogin(@Valid @RequestBody LoginRequestDto loginRequestDto) {
 		try {
 			LoginResponseDto loginResponseDto = userService.login(loginRequestDto);
-			if (loginResponseDto.responseMsg().equals(LoginResponseMsg.SUCCESS.getResponseMsg())) {
-				return ResponseEntity.ok().body(loginResponseDto);
-			} else {
-				return ResponseEntity.badRequest().body(loginResponseDto);
-			}
-		} catch (HttpClientErrorException e) {
-			LoginResponseDto loginResponseDto = LoginResponseDto.builder()
-				.responseMsg(LoginResponseMsg.USER_NOT_FOUND.getResponseMsg())
-				.build();
-			return ResponseEntity.status(e.getStatusCode()).body(loginResponseDto);
+			return ResponseEntity.ok().body(loginResponseDto);
+		} catch (RuntimeException e) {
+			throw new UserException(USER_SERVER_ERROR);
 		}
 	}
 }
