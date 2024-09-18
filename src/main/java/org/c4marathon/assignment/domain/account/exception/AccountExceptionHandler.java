d239ddf0ef3b020ac1858b149f35684c0c08dba3
조아빈 <whdkqls122@naver.com>
2024-09-18 17:43:51 +0900

    [refactor] : RuntimeException AccountExceptionHandler에서 잡도록 수정

--- src/main/java/org/c4marathon/assignment/domain/account/exception/AccountExceptionHandler.java
+++ src/main/java/org/c4marathon/assignment/domain/account/exception/AccountExceptionHandler.java
@@ -1,7 +1,12 @@
 package org.c4marathon.assignment.domain.account.exception;
 
+import static org.c4marathon.assignment.domain.account.entity.AccountErrCode.*;
+
 import org.c4marathon.assignment.domain.account.controller.AccountController;
 import org.c4marathon.assignment.domain.account.dto.AccountErrDto;
+import org.c4marathon.assignment.domain.account.entity.AccountErrCode;
+import org.c4marathon.assignment.domain.user.dto.UserErrDto;
+import org.c4marathon.assignment.domain.user.entity.UserErrCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,15 +16,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AccountExceptionHandler {
 	@ExceptionHandler({AccountException.class})
 	protected ResponseEntity<AccountErrDto> handleAccountException(AccountException ex) {
+		return getAccountErrDto(ex.getAccountErrCode());
+	}
+
+	@ExceptionHandler({RuntimeException.class})
+	protected ResponseEntity<AccountErrDto> handleAccountRuntimeException(){
+		return(getAccountErrDto(ACCOUNT_SERVER_ERROR));
+	}
+
+	private ResponseEntity<AccountErrDto> getAccountErrDto(AccountErrCode errCode) {
 		HttpStatus httpStatus;
 		try {
-			httpStatus = HttpStatus.valueOf(ex.getAccountErrCode().getStatus());
+			httpStatus = HttpStatus.valueOf(errCode.getStatus());
 		} catch (IllegalAccessError e) {
 			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 		AccountErrDto errDto = new AccountErrDto(
-			ex.getAccountErrCode().getStatus(),
-			ex.getAccountErrCode().getMessage(),
+			errCode.getStatus(),
+			errCode.getMessage(),
 			httpStatus
 		);
 		return new ResponseEntity<>(errDto, httpStatus);
