5732c4b7046235e32035d2075ac6452a417c9bdc
조아빈 <whdkqls122@naver.com>
2024-09-18 22:26:23 +0900

    [refactor] : enum 반환메시지 통일성있게 수정

--- src/main/java/org/c4marathon/assignment/domain/user/entity/UserErrCode.java
+++ src/main/java/org/c4marathon/assignment/domain/user/entity/UserErrCode.java
@@ -9,10 +9,10 @@ public enum UserErrCode {
 	USER_INVALID_FAIL(400, JoinResponseMsg.INVALID_FAIL.getResponseMsg()),
 	USER_DUPLICATED_FAIL(400, JoinResponseMsg.DUPLICATEDFAIL.getResponseMsg()),
 	USER_LOGIN_FAIL(400, LoginResponseMsg.FAIL.getResponseMsg()),
-	USER_SESSION_ERR(401, "다시 로그인 해주세요."),
+	USER_SESSION_ERR(401, LoginResponseMsg.USER_RE_LOGIN.getResponseMsg()),
 
 	USER_NOT_FOUND(404, LoginResponseMsg.USER_NOT_FOUND.getResponseMsg()),
-	USER_SERVER_ERROR(500, "서버에러 입니다. 다시 시도 해주세요.");
+	USER_SERVER_ERROR(500, LoginResponseMsg.USER_SERVER_ERROR.getResponseMsg());
 	private final int status;
 	private final String message;
 }
