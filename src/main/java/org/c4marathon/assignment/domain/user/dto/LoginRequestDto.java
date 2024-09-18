9d79fd82d9137c5d5003c65a239f035fdc7c90b0
조아빈 <whdkqls122@naver.com>
2024-09-18 23:12:39 +0900

    [feat] : valid 메시지 추가

--- src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
@@ -6,5 +6,5 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public record LoginRequestDto(
-	@NotBlank String userPhone,
-	@NotBlank String userPassword) {}
+	@NotBlank(message = "전화번호는 공백일 수 없습니다.") String userPhone,
+	@NotBlank(message = "비밀번호는 공백일 수 없습니다.") String userPassword) {}
--- src/main/java/org/c4marathon/assignment/domain/user/dto/UserDto.java
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/UserDto.java
@@ -3,8 +3,8 @@ package org.c4marathon.assignment.domain.user.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserDto(
-	@NotBlank String userPhone,
-	@NotBlank String userPassword,
-	@NotBlank String userName,
-	@NotBlank String userBirth
+	@NotBlank(message = "전화번호는 공백일 수 없습니다.") String userPhone,
+	@NotBlank(message = "비밀번호는 공백일 수 없습니다.") String userPassword,
+	@NotBlank(message = "이름은 공백일 수 없습니다.") String userName,
+	@NotBlank(message = "생년월일은 공백일 수 없습니다.") String userBirth
 ) {}
