9562a60bb00570f4c14665b2e078cd593186c2da
조아빈 <whdkqls122@naver.com>
2024-09-18 22:29:31 +0900

    [refactor] : enum 반환메시지 통일성있게 수정

--- src/main/java/org/c4marathon/assignment/domain/account/entity/AccountErrCode.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/AccountErrCode.java
@@ -10,13 +10,13 @@ public enum AccountErrCode {
 	ACCOUNT_UNAVAILABLE(400, AccountStatus.UNAVAILABLE.getAccountStatus()),
 	ACCOUNT_DALIYCHARGELIMIT_ERR(400, RemittanceResponseMsg.DAILYCHARGELIMIT_ERR.getResponseMsg()),
 	ACCOUNT_INSUFFICIENT_BALANCE(400, RemittanceResponseMsg.INSUFFICIENT_BALANCE.getResponseMsg()),
-	INVALID_ACCOUNT_TYPE(400, "잘못된 계좌타입 입니다."),
+	INVALID_ACCOUNT_TYPE(400, CreateResponseMsg.INVALID_ACCOUNT_TYPE.getResponseMsg()),
 
 	ACCOUNT_USER_NOT_FOUND(404, CreateResponseMsg.NOUSER.getResponseMsg()),
 	ACCOUNT_NOT_FOUND(404, RemittanceResponseMsg.NOSUCHACCOUNT.getResponseMsg()),
 
 	ACCOUNT_CREATE_FAIL(500, CreateResponseMsg.FAIL.getResponseMsg()),
-	ACCOUNT_SERVER_ERROR(500, "서버 에러 입니다. 다시 시도 부탁드립니다.");
+	ACCOUNT_SERVER_ERROR(500, CreateResponseMsg.ACCOUNT_SERVER_ERROR.getResponseMsg());
 	private final int status;
 	private final String message;
 }
--- src/main/java/org/c4marathon/assignment/domain/account/entity/CreateResponseMsg.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/CreateResponseMsg.java
@@ -9,7 +9,9 @@ import lombok.Getter;
 public enum CreateResponseMsg {
 	SUCCESS("계좌생성 성공!"),
 	FAIL("계좌생성 실패"),
-	NOUSER("유저 정보가 없습니다.");
+	NOUSER("유저 정보가 없습니다."),
+	INVALID_ACCOUNT_TYPE("잘못된 계좌 유형입니다."),
+	ACCOUNT_SERVER_ERROR("서버 에러 입니다. 다시 시도 부탁드립니다.");
 
 	private final String responseMsg;
 }
