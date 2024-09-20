a49646569a2af2c68e78a8eafd3b6f6fa91a77cf
dudxo <father8475@naver.com>
2024-09-20 22:03:11 +0900

    [sytle] : 오탈자 수정

--- src/main/java/org/c4marathon/assignment/account/exception/AccountErrorCode.java
+++ src/main/java/org/c4marathon/assignment/account/exception/AccountErrorCode.java
@@ -12,7 +12,7 @@ public enum AccountErrorCode implements ErrorCode {
 	NOT_FOUND_ACCOUNT_TYPE("해당 계좌 타입을 찾을 수 없습니다.", "ACCOUNT_001"),
 	NOT_FOUND_ACCOUNT("계좌를 찾을 수 없습니다.", "ACCOUNT_002"),
 	NOT_ENOUGH_AMOUNT("계좌 금액이 충분하지 않습니다.", "ACCOUNT_003"),
-	NOT_AUTHORIZED_ACCOUNT("계좌 인출 권환이 없습니다.", "ACCOUNT_004"),
+	NOT_AUTHORIZED_ACCOUNT("계좌 인출 권한이 없습니다.", "ACCOUNT_004"),
 	NOT_ENOUGH_CHARGE_AMOUNT("충전 한도를 초과했습니다.", "ACCOUNT_005"),
 	NOT_ACCESS_CHARGE("계좌 금액 충전에 접근할 수 없는 계좌입니다.", "ACCOUNT_006");
 
