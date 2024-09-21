e3205290910bfeb730f00f73f9a64c7d83ae5b59
jwj9127 <jwj9127@naver.com>
2024-09-21 18:58:28 +0900

    [fix]: 조건문 재확인 코드 제거

---
+++ src/main/java/org/c4marathon/assignment/event/JoinEventHandler.java
@@ -0,0 +1,37 @@
+package org.c4marathon.assignment.common.config;
+
+import java.util.Random;
+
+import org.c4marathon.assignment.account.domain.Account;
+import org.c4marathon.assignment.account.domain.AccountType;
+import org.c4marathon.assignment.account.repository.AccountRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class JoinEventHandler {
+	private final AccountRepository accountRepository;
+
+	Long random = new Random().nextLong();
+
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void craeteAccount(JoinEventDto joinEventDto) {
+		Account account = Account.builder()
+			.accountNum(random)
+			.type(AccountType.MAIN_ACCOUNT)
+			.accountPw(1234)
+			.limitaccount(3000000)
+			.amount(0)
+			.user(joinEventDto.getUser())
+			.build();
+
+		accountRepository.save(account);
+	}
+}
--- src/main/java/org/c4marathon/assignment/user/controller/UserController.java
+++ src/main/java/org/c4marathon/assignment/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package org.c4marathon.assignment.user.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.c4marathon.assignment.config.CommonResponse;
+import org.c4marathon.assignment.common.config.CommonResponse;
 import org.c4marathon.assignment.user.dto.JoinDto;
 import org.c4marathon.assignment.user.dto.LoginDto;
 import org.c4marathon.assignment.user.service.UserService;
@@ -11,66 +10,65 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    // 회원가입
-    @PostMapping("user/join")
-    public ResponseEntity<CommonResponse> join(
-            @RequestBody JoinDto joinDto
-    ){
+	// 회원가입
+	@PostMapping("user/join")
+	public ResponseEntity<CommonResponse> join(
+		@RequestBody JoinDto joinDto
+	) {
 
-        boolean checkJoin = userService.save(joinDto);
+		boolean checkJoin = userService.save(joinDto);
 
-        if(checkJoin == true){
-            CommonResponse res = new CommonResponse(
-                    200,
-                    HttpStatus.OK,
-                    "회원가입 성공",
-                    null
-            );
-            return new ResponseEntity<>(res, res.getHttpStatus());
-        }else {
-            CommonResponse res = new CommonResponse(
-                    400,
-                    HttpStatus.BAD_REQUEST,
-                    "회원가입 실패",
-                    null
-            );
-            return new ResponseEntity<>(res, res.getHttpStatus());
-        }
-    }
+		if (checkJoin) {
+			CommonResponse res = new CommonResponse(
+				200,
+				HttpStatus.OK,
+				"회원가입 성공",
+				null
+			);
+			return new ResponseEntity<>(res, res.getHttpStatus());
+		} else {
+			CommonResponse res = new CommonResponse(
+				400,
+				HttpStatus.BAD_REQUEST,
+				"회원가입 실패",
+				null
+			);
+			return new ResponseEntity<>(res, res.getHttpStatus());
+		}
+	}
 
+	// 로그인
+	@PostMapping("user/login")
+	public ResponseEntity<CommonResponse> login(
+		@RequestBody LoginDto loginDto
+	) {
+		LoginDto loginResult = userService.login(loginDto);
 
-    // 로그인
-    @PostMapping("user/login")
-    public ResponseEntity<CommonResponse> login(
-            @RequestBody LoginDto loginDto
-    ){
-        LoginDto loginResult = userService.login(loginDto);
-
-        if(loginResult != null){
-            CommonResponse res = new CommonResponse(
-                    200,
-                    HttpStatus.OK,
-                    "로그인 성공",
-                    null
-            );
-            return new ResponseEntity<>(res, res.getHttpStatus());
-        }else {
-            CommonResponse res = new CommonResponse(
-                    400,
-                    HttpStatus.BAD_REQUEST,
-                    "로그인 실패",
-                    null
-            );
-            return new ResponseEntity<>(res, res.getHttpStatus());
-        }
-    }
+		if (loginResult != null) {
+			CommonResponse res = new CommonResponse(
+				200,
+				HttpStatus.OK,
+				"로그인 성공",
+				null
+			);
+			return new ResponseEntity<>(res, res.getHttpStatus());
+		} else {
+			CommonResponse res = new CommonResponse(
+				400,
+				HttpStatus.BAD_REQUEST,
+				"로그인 실패",
+				null
+			);
+			return new ResponseEntity<>(res, res.getHttpStatus());
+		}
+	}
 
 }
