74741f89b95719882657e400277eca4fb80821e9
jwj9127 <jwj9127@naver.com>
2024-09-21 19:01:46 +0900

    [fix]: 회원가입 이벤트리스너 코드 추가 및 로그인 회원가입 조건문 삭제 후 예외처리 기능 추가

--- src/main/java/org/c4marathon/assignment/user/service/UserService.java
+++ src/main/java/org/c4marathon/assignment/user/service/UserService.java
@@ -1,53 +1,49 @@
 package org.c4marathon.assignment.user.service;
 
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.common.exception.BaseException;
 import org.c4marathon.assignment.user.domain.User;
 import org.c4marathon.assignment.user.dto.JoinDto;
 import org.c4marathon.assignment.user.dto.LoginDto;
+import org.c4marathon.assignment.user.exception.JoinException;
+import org.c4marathon.assignment.user.exception.LoginException;
 import org.c4marathon.assignment.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionalEventListener;
 
-import java.util.Optional;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    // 회원가입
-    @Transactional
-    public boolean save(JoinDto joinDto){
-        Optional checkUser = userRepository.findByUserId(joinDto.userId());
+	// 회원가입
+	@Transactional
+	@TransactionalEventListener
+	public boolean save(JoinDto joinDto) {
+		userRepository.findByUserId(joinDto.userId()).orElseThrow(() -> new BaseException(JoinException.FOUND_USER));
 
-        if(checkUser == null){
-            User user = User.builder()
-                    .userId(joinDto.userId())
-                    .userPw(joinDto.userPw())
-                    .name(joinDto.name())
-                    .build();
+		User user = User.builder()
+			.userId(joinDto.userId())
+			.userPw(joinDto.userPw())
+			.name(joinDto.name())
+			.build();
 
-            userRepository.save(user);
-            return true;
-        }else {
-            return false;
-        }
-    }
+		userRepository.save(user);
+		return true;
+	}
 
-    // 로그인
-    public LoginDto login(LoginDto loginDto){
-        Optional<User> userRepositoryByUserId = userRepository.findByUserId(loginDto.userId());
-        if (userRepositoryByUserId.isPresent()) {
-            User userEntity = userRepositoryByUserId.get();
+	// 로그인
+	public LoginDto login(LoginDto loginDto) {
+		User userEntity = userRepository.findByUserId(loginDto.userId())
+			.orElseThrow(() -> new BaseException(LoginException.NOT_FOUND_USER));
 
-            if (userEntity.getUserPw().equals(loginDto.userPw())) {
-                return loginDto;
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
+		if (userEntity.getUserPw().equals(loginDto.userPw())) {
+			return loginDto;
+		} else {
+			throw new BaseException(LoginException.NOT_MATCH_PASSWORD);
+		}
+	}
 }
