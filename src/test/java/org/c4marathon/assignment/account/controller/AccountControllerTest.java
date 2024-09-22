a189e65953dfae2771440941c0b7fa2ccb67090f
조아빈 <whdkqls122@naver.com>
2024-09-22 17:00:58 +0900

    [test] : 인증된 가짜 사용자를 사용할 수 있도록 WithMockUser 추가 및 주석 제거

--- src/test/java/org/c4marathon/assignment/account/controller/AccountControllerTest.java
+++ src/test/java/org/c4marathon/assignment/account/controller/AccountControllerTest.java
@@ -3,6 +3,7 @@ package org.c4marathon.assignment.account.controller;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -13,6 +14,7 @@ import org.c4marathon.assignment.domain.account.dto.CreateResponseDto;
 import org.c4marathon.assignment.domain.account.dto.RemittanceRequestDto;
 import org.c4marathon.assignment.domain.account.dto.RemittanceResponseDto;
 import org.c4marathon.assignment.domain.account.dto.SavingRequestDto;
+import org.c4marathon.assignment.domain.account.entity.CreateResponseMsg;
 import org.c4marathon.assignment.domain.account.entity.RemittanceResponseMsg;
 import org.c4marathon.assignment.domain.account.service.AccountService;
 import org.junit.jupiter.api.DisplayName;
@@ -26,6 +28,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -33,6 +36,7 @@ import jakarta.servlet.http.HttpSession;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(username = "testUser", roles = {"USER"})
 public class AccountControllerTest {
 
 	@Autowired
@@ -58,33 +62,34 @@ public class AccountControllerTest {
 		mockMvc.perform(post("/account/remittance/{userId}", userId)  // POST 요청
 				.contentType(MediaType.APPLICATION_JSON)  // 요청 본문의 타입 지정 (JSON)
 				.content(objectMapper.writeValueAsString(remittanceRequestDto))  // 요청 본문에 remittanceRequestDto를 JSON으로 변환하여 전달
+				.with(csrf())
 				.characterEncoding("utf-8"))
 			.andExpect(status().isOk())  // 응답 상태 코드가 200인지 확인
 			.andExpect(jsonPath("$.responseMsg").value(RemittanceResponseMsg.SUCCESS.getResponseMsg()));  // 응답 본문에 있는 responseMsg 확인
 	}
+	@DisplayName("적금계좌 생성 api 테스트")
+	@Test
+	public void testCreateAccount() throws Exception {
+		CreateResponseDto responseDto = new CreateResponseDto(CreateResponseMsg.SUCCESS.getResponseMsg());
+		when(accountService.createAccountOther(anyLong(), anyString(), any())).thenReturn(responseDto);
+
+		mockMvc.perform(post("/account/creataccount/role/1")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(new ObjectMapper().writeValueAsString(responseDto))
+		).andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("적금계좌 입급 api 테스트")
+	public void testSavingRemittance() throws Exception {
+		SavingRequestDto requestDto = new SavingRequestDto(30000);
+		RemittanceResponseDto remittanceResponseDto = new RemittanceResponseDto(RemittanceResponseMsg.SUCCESS.getResponseMsg());
+		when(accountService.savingRemittance(anyLong(), any(SavingRequestDto.class), any())).thenReturn(remittanceResponseDto);
+
+		mockMvc.perform(post("/account/saving/1")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(new ObjectMapper().writeValueAsString(requestDto))
+		).andExpect(status().isOk());
+	}
 }
-	// @DisplayName("적금계좌 생성 api 테스트")
-	// @Test
-	// public void testCreateAccount() throws Exception {
-	// 	CreateResponseDto responseDto = CreateResponseDto.builder().build();
-	// 	when(accountService.createAccountOther(anyLong(), anyString(), any())).thenReturn(responseDto);
-	//
-	// 	mockMvc.perform(post("/account/creataccount/role/1")
-	// 		.contentType(MediaType.APPLICATION_JSON)
-	// 		.content(new ObjectMapper().writeValueAsString(responseDto))
-	// 	).andExpect(status().isOk());
-	// }
-	//
-	// @Test
-	// @DisplayName("적금계좌 입급 api 테스트")
-	// public void testSavingRemittance() throws Exception {
-	// 	SavingRequestDto requestDto = SavingRequestDto.builder().build();
-	// 	RemittanceResponseDto responseDto = RemittanceResponseDto.builder().build();
-	// 	when(accountService.savingRemittance(anyLong(), any(SavingRequestDto.class), any())).thenReturn(responseDto);
-	//
-	// 	mockMvc.perform(post("/account/saving/1")
-	// 			.contentType(MediaType.APPLICATION_JSON)
-	// 			.content(new ObjectMapper().writeValueAsString(requestDto))
-	// 	).andExpect(status().isOk());
-	// }
-// }
+
