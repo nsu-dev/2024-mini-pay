3f6fef50e9812c7cb62141605fe0d13e58f3c1e9
조아빈 <whdkqls122@naver.com>
2024-09-22 15:56:19 +0900

    [style] 줄바꿈 정리

--- src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/user/dto/LoginRequestDto.java
@@ -6,5 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public record LoginRequestDto(
-	@NotBlank(message = "전화번호는 공백일 수 없습니다.") String userPhone,
-	@NotBlank(message = "비밀번호는 공백일 수 없습니다.") String userPassword) {}
+	@NotBlank(message = "전화번호는 공백일 수 없습니다.")
+	String userPhone,
+	@NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+	String userPassword
+) {}
