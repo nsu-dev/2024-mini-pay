a7753af66330c2b0b2aea765266654316eb2cf60
jwj9127 <jwj9127@naver.com>
2024-09-23 15:14:00 +0900

    [feat]: 계좌 비밀번호 관련 로직 추가

--- src/main/java/org/c4marathon/assignment/user/dto/JoinDto.java
+++ src/main/java/org/c4marathon/assignment/user/dto/JoinDto.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.user.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record JoinDto (
@@ -10,6 +12,11 @@ public record JoinDto (
         String userPw,
 
         @NotBlank(message = "이름은 필수 입력 항목입니다.")
-        String name
+        String name,
+
+        @NotBlank(message = "계좌 비밀번호는 필수 입력 항목입니다.")
+        @Min(value = 1000, message = "비밀번호는 0으로 시작할 수 없습니다")
+        @Max(value = 9999, message = "비밀번호가 4자리를 넘어갈 수 없습니다")
+        int accountPw
 ){
 }
