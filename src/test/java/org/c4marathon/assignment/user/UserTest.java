a71ebf84538d8acc830439d35e6cca4952713faf
jwj9127 <jwj9127@naver.com>
2024-09-24 01:00:57 +0900

    [fix]: 회원가입, 로그인 테스트 기능 완성

--- src/test/java/org/c4marathon/assignment/user/UserTest.java
+++ src/test/java/org/c4marathon/assignment/user/UserTest.java
@@ -1,7 +1,12 @@
 package org.c4marathon.assignment.user;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
+
+import org.c4marathon.assignment.user.domain.User;
 import org.c4marathon.assignment.user.dto.JoinDto;
 import org.c4marathon.assignment.user.dto.LoginDto;
 import org.c4marathon.assignment.user.repository.UserRepository;
@@ -16,60 +21,63 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserTest {
 
-    @MockBean
-    private UserRepository userRepository;
+	@MockBean
+	private UserRepository userRepository;
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    private String toJson(Object object) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(object);
-    }
+	private String toJson(Object object) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(object);
+	}
 
-    @BeforeEach
-    void start() {
-        userRepository.deleteAll();
-    }
+	@BeforeEach
+	void start() {
+		userRepository.deleteAll();
+	}
 
-    @AfterEach
-    void end() {
-        userRepository.deleteAll();
-    }
+	@AfterEach
+	void end() {
+		userRepository.deleteAll();
+	}
 
-    @DisplayName("[회원가입 테스트]")
-    @Test
-    void joinTest() throws Exception {
-        // given
-        JoinDto joinDto = new JoinDto("abcd", "a1234", "홍길동");
+	@DisplayName("[회원가입 테스트]")
+	@Test
+	void joinTest() throws Exception {
+		// given
+		JoinDto joinDto = new JoinDto("aaaa", "ab12", "홍길동", 1234);
 
-        // when, then
-        mockMvc.perform(post("/user/join")
-                        .content(toJson(joinDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+		// when, then
+		mockMvc.perform(post("/user/join")
+				.content(toJson(joinDto))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
 
-    @DisplayName("[로그인 테스트]")
-    @Test
-    void loginTest() throws Exception {
+	@DisplayName("[로그인 테스트]")
+	@Test
+	void loginTest() throws Exception {
 
-        // given
-        LoginDto loginDto = new LoginDto("abcd", "a1234");
+		// given
+		LoginDto loginDto = new LoginDto("abcd", "a1234");
 
-        // when, then
-        mockMvc.perform(post("/user/login")
-                        .content(toJson(loginDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+		User user = new User("abcd", "a1234", "홍길동", 1234);
+		given(userRepository.findByUserId("abcd")).willReturn(Optional.of(user));
+
+		// when, then
+		mockMvc.perform(post("/user/login")
+				.content(toJson(loginDto))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
 }
