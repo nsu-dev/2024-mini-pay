07a74c0ad8ba00704e0830e4889293307943d322
jwj9127 <jwj9127@naver.com>
2024-09-24 01:03:35 +0900

    @Query메서드를 이용해 직관적인 메서드 네이밍 수정

--- src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
+++ src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
@@ -1,20 +1,21 @@
 package org.c4marathon.assignment.account.repository;
 
+import java.util.Optional;
+
 import org.c4marathon.assignment.account.domain.Account;
 import org.c4marathon.assignment.account.domain.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional findByAccount(Long accountNum);
+	@Query("SELECT a FROM Account a WHERE a.accountNum = :accountNum")
+	Optional findByAccount(Long accountNum);
 
-    Optional findByMyAccount(int accountPw);
+	@Query("SELECT a FROM Account a WHERE a.accountPw = :accountPw")
+	Optional findByMyAccount(int accountPw);
 
-    Optional<Account> findByUser_IdAndType(Long userId, AccountType type);
+	Optional<Account> findByUser_IdAndType(Long userId, AccountType type);
 
 }
