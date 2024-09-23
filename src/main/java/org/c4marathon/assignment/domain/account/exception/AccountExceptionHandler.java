959b92abae8e5d4bcb06869cd5215d52f7eb862d
조아빈 <whdkqls122@naver.com>
2024-09-23 17:36:16 +0900

    [feat] : 공백인 경우 validation예외처리 추가

--- src/main/java/org/c4marathon/assignment/domain/account/exception/AccountExceptionHandler.java
+++ src/main/java/org/c4marathon/assignment/domain/account/exception/AccountExceptionHandler.java
@@ -1,12 +1,19 @@
 package org.c4marathon.assignment.domain.account.exception;
 
 import static org.c4marathon.assignment.domain.account.entity.AccountErrCode.*;
+import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.c4marathon.assignment.domain.account.controller.AccountController;
 import org.c4marathon.assignment.domain.account.dto.AccountErrDto;
 import org.c4marathon.assignment.domain.account.entity.AccountErrCode;
+import org.c4marathon.assignment.domain.user.dto.UserErrDto;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,13 +23,24 @@ public class AccountExceptionHandler {
 	protected ResponseEntity<AccountErrDto> handleAccountException(AccountException ex) {
 		return getAccountErrDto(ex.getAccountErrCode());
 	}
+	@ExceptionHandler({MethodArgumentNotValidException.class})
+	protected ResponseEntity<AccountErrDto> handleUserInvalidException(MethodArgumentNotValidException ex) {
+		List<String> invalidMsgList = ex.getBindingResult().getFieldErrors().stream()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.collect(Collectors.toList());
 
+		String invalidMsg = String.join(", ", invalidMsgList);
+		return getAccountErrDto(ACCOUNT_INVALID_FAIL, invalidMsg);
+	}
 	@ExceptionHandler({RuntimeException.class})
 	protected ResponseEntity<AccountErrDto> handleAccountRuntimeException(){
 		return(getAccountErrDto(ACCOUNT_SERVER_ERROR));
 	}
 
-	private ResponseEntity<AccountErrDto> getAccountErrDto(AccountErrCode errCode) {
+	private ResponseEntity<AccountErrDto> getAccountErrDto(AccountErrCode errCode){
+		return getAccountErrDto(errCode, errCode.getMessage());
+	}
+	private ResponseEntity<AccountErrDto> getAccountErrDto(AccountErrCode errCode , String errMsg) {
 		HttpStatus httpStatus;
 		try {
 			httpStatus = HttpStatus.valueOf(errCode.getStatus());
@@ -31,7 +49,7 @@ public class AccountExceptionHandler {
 		}
 		AccountErrDto errDto = new AccountErrDto(
 			errCode.getStatus(),
-			errCode.getMessage(),
+			errMsg != null ? errMsg : errCode.getMessage(),
 			httpStatus
 		);
 		return new ResponseEntity<>(errDto, httpStatus);
