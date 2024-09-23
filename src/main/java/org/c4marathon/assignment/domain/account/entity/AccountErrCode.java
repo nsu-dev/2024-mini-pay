177d1e7d5b10b3db9a9370af792e2b2f46b650a8
조아빈 <whdkqls122@naver.com>
2024-09-23 17:33:40 +0900

    [feat] : 공백인 경우 예외 enum 추가

--- src/main/java/org/c4marathon/assignment/domain/account/entity/AccountErrCode.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/AccountErrCode.java
@@ -12,6 +12,7 @@ public enum AccountErrCode {
 	ACCOUNT_INSUFFICIENT_BALANCE(400, RemittanceResponseMsg.INSUFFICIENT_BALANCE.getResponseMsg()),
 	INVALID_ACCOUNT_TYPE(400, CreateResponseMsg.INVALID_ACCOUNT_TYPE.getResponseMsg()),
 	NOT_MAIN_ACCOUNT(400, RemittanceResponseMsg.NOT_MAIN_ACCOUNT.getResponseMsg()),
+	ACCOUNT_INVALID_FAIL(400, RemittanceResponseMsg.INVALID_FAIL.getResponseMsg()),
 
 	ACCOUNT_USER_NOT_FOUND(404, CreateResponseMsg.NOUSER.getResponseMsg()),
 	ACCOUNT_NOT_FOUND(404, RemittanceResponseMsg.NOSUCHACCOUNT.getResponseMsg()),
