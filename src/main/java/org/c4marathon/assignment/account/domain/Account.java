457d2ac7427d93e5628dcd50e4cf2e146fcb26cd
jwj9127 <jwj9127@naver.com>
2024-09-23 15:03:15 +0900

    [fix]: 계좌번호 unique 추가, 한도 기능 추가, 예외처리 커스텀 예외로 변경, 송금 후 금액 추가, 삭감 메서드명 변경

--- src/main/java/org/c4marathon/assignment/account/domain/Account.java
+++ src/main/java/org/c4marathon/assignment/account/domain/Account.java
@@ -1,14 +1,24 @@
 package org.c4marathon.assignment.account.domain;
 
-import jakarta.persistence.*;
+import org.c4marathon.assignment.account.exception.MainAccountException;
+import org.c4marathon.assignment.common.exception.BaseException;
+import org.c4marathon.assignment.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.exception.UncheckedException;
-import org.c4marathon.assignment.user.domain.User;
-
-import java.util.Date;
 
 @Entity
 @NoArgsConstructor
@@ -18,44 +28,48 @@ import java.util.Date;
 @Builder
 public class Account {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "account_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "account_id")
+	private Long id;
 
-    @Column(name = "accountNumber", nullable = false)
-    private Long accountNum;
+	@Column(name = "accountNumber", nullable = false, unique = true)
+	private Long accountNum;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "account_type", nullable = false)
-    private AccountType type;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "account_type", nullable = false)
+	private AccountType type;
 
-    @Column(name = "amount", nullable = false)
-    private int amount;
+	@Column(name = "amount", nullable = false)
+	private int amount;
 
-    @Column(name = "accountPw", nullable = false)
-    private int accountPw;
+	@Column(name = "accountPw", nullable = false)
+	private int accountPw;
 
-    @Column(name = "limitaccount", nullable = false)
-    private int limitaccount;
+	@Column(name = "limitaccount", nullable = false)
+	private int limitaccount;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "users_id")
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "users_id")
+	private User user;
 
-    public void setAmount(int updatedAmount) {
-        this.amount += updatedAmount;
-        this.limitaccount -= updatedAmount;
-        if(this.limitaccount < 0){
-            throw new NullPointerException();
-        }
-    }
+	public void resetLimitAccount() {
+		this.limitaccount = 3000000;
+	}
 
-    public void plusAmount(int updatedAmount) {
-        this.amount += updatedAmount;
-    }
+	public void setAmount(int updatedAmount) {
+		this.amount += updatedAmount;
+		this.limitaccount -= updatedAmount;
+		if (this.limitaccount < 0) {
+			throw new BaseException(MainAccountException.LIMIT_ACCOUNT);
+		}
+	}
 
-    public void minusAmount(int updatedAmount) {
-        this.amount -= updatedAmount;
-    }
+	public void increaseAmount(int updatedAmount) {
+		this.amount += updatedAmount;
+	}
+
+	public void reduceAmount(int updatedAmount) {
+		this.amount -= updatedAmount;
+	}
 }
