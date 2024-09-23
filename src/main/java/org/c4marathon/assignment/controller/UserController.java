1e26ce2ce513242754fade5f4f47e6e7ac6fc881
Lee Soo Kyung <dltnrud377@naver.com>
2024-09-23 23:05:03 +0900

    [feat]: 컨트롤러에 큐 추가

--- src/main/java/org/c4marathon/assignment/controller/UserController.java
+++ src/main/java/org/c4marathon/assignment/controller/UserController.java
@@ -1,8 +1,10 @@
 package org.c4marathon.assignment.controller;
 
+import org.c4marathon.assignment.Dto.TransferRequestDto;
 import org.c4marathon.assignment.Dto.UserRequestDto;
 import org.c4marathon.assignment.Dto.UserResponseDto;
 import org.c4marathon.assignment.service.AccountService;
+import org.c4marathon.assignment.service.QueueService;
 import org.c4marathon.assignment.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,7 @@ public class UserController {
 
 	private final UserService userService;
 	private final AccountService accountService;
+	private final QueueService queueService;
 
 	//사용자 회원가입
 	@PostMapping("/register")
@@ -45,8 +48,9 @@ public class UserController {
 	public ResponseEntity<String> transferToSavings(@PathVariable Long userId,
 		@RequestParam Long savingsAccountId,
 		@RequestParam int money) {
-		boolean success = accountService.transferToSavings(userId, savingsAccountId, money);
-		return success ? ResponseEntity.ok("송금 성공") : ResponseEntity.badRequest().body("송금 실패");
+		TransferRequestDto request = new TransferRequestDto(userId, savingsAccountId, money);
+		queueService.addToQueue(request);
+		return ResponseEntity.ok("송금 요청이 접수되었습니다.");
 	}
 
 	//외부 계좌에서 사용자 계좌로 입금(메인 계좌)
@@ -55,14 +59,9 @@ public class UserController {
 		@PathVariable Long userId,
 		@PathVariable Long externalUserId,
 		@RequestParam int money) {
-		try {
-			boolean success = accountService.transferFromExternalAccount(userId, externalUserId, money);
-			return success ? ResponseEntity.ok("이체 성공") : ResponseEntity.badRequest().body("이체 실패");
-		} catch (IllegalArgumentException e) {
-			return ResponseEntity.badRequest().body("오류: " + e.getMessage());
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류: " + e.getMessage());
-		}
+		TransferRequestDto request = new TransferRequestDto(userId, externalUserId, money);
+		queueService.addToQueue(request);
+		return ResponseEntity.ok("이체 요청이 접수되었습니다.");
 	}
 
 	// IllegalArgumentException 처리
