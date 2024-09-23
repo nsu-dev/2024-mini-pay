027556f0c33b582f1bc79e7e92cf7ddb321f062e
jwj9127 <jwj9127@naver.com>
2024-09-24 00:55:40 +0900

    [fix]: test 코드 메인 계좌 충전 테스트 완성, 적금 계좌 생성 테스트 구현중

--- src/test/java/org/c4marathon/assignment/account/AccountTest.java
+++ src/test/java/org/c4marathon/assignment/account/AccountTest.java
@@ -1,10 +1,20 @@
 package org.c4marathon.assignment.account;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
+
+import org.c4marathon.assignment.account.domain.Account;
+import org.c4marathon.assignment.account.domain.AccountType;
 import org.c4marathon.assignment.account.dto.ChargeDto;
+import org.c4marathon.assignment.account.dto.SavingAccountPwDto;
 import org.c4marathon.assignment.account.dto.SendDto;
 import org.c4marathon.assignment.account.repository.AccountRepository;
+import org.c4marathon.assignment.user.domain.User;
+import org.c4marathon.assignment.user.dto.JoinDto;
+import org.c4marathon.assignment.user.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,78 +26,98 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Random;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AccountTest {
-    @MockBean
-    private AccountRepository accountRepository;
 
-    @Autowired
-    private MockMvc mockMvc;
+	@MockBean
+	private UserRepository userRepository;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@MockBean
+	private AccountRepository accountRepository;
 
-    private String toJson(Object object) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(object);
-    }
+	@Autowired
+	private MockMvc mockMvc;
 
-    @BeforeEach
-    void start() {
-        accountRepository.deleteAll();
-    }
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    @AfterEach
-    void end() {
-        accountRepository.deleteAll();
-    }
+	private String toJson(Object object) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(object);
+	}
 
-    @DisplayName("[메인 계좌 충전 테스트]")
-    @Test
-    void chargeTest() throws Exception {
-        Long random = new Random().nextLong();
-        // given
-        ChargeDto chargeDto = new ChargeDto(random, 10000);
+	@BeforeEach
+	void start() {
+		accountRepository.deleteAll();
+	}
 
-        // when, then
-        mockMvc.perform(post("/account/charge")
-                        .content(toJson(chargeDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+	@AfterEach
+	void end() {
+		accountRepository.deleteAll();
+	}
 
-    @DisplayName("[적금 계좌 생성 테스트]")
-    @Test
-    void savingTest() throws Exception {
+	@DisplayName("[메인 계좌 충전 테스트]")
+	@Test
+	void chargeTest() throws Exception {
+		// 회원가입 후 계좌 생성
+		User user = new User("user123", "password", "홍길동", 1234);
+		Account mainAccount = new Account(12345678L, AccountType.MAIN_ACCOUNT, 0, 1234, 3000000, user);
 
-        // given
-        Long userId = 1L;
+		// Mockito: accountRepository에서 특정 계좌번호로 조회될 때 반환값 설정
+		given(accountRepository.findByAccount(12345678L)).willReturn(Optional.of(mainAccount));
 
-        // when, then
-        mockMvc.perform(post("/account/create/{userId}")
-                        .content(toJson(userId))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+		// 메인 계좌 가져오기
+		Optional<Account> account = accountRepository.findByAccount(12345678L);
 
-    @DisplayName("[송금 테스트]")
-    @Test
-    void sendTest() throws Exception {
+		// given: 충전할 계좌 번호와 금액
+		ChargeDto chargeDto = new ChargeDto(account.get().getAccountNum(), 10000);
 
-        // given
-        Long userId = 1L;
-        SendDto loginDto = new SendDto(11111111L, 5000, 1234);
+		// when, then: 해당 계좌에 충전 테스트 수행
+		mockMvc.perform(post("/account/charge")
+				.content(toJson(chargeDto))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
 
-        // when, then
-        mockMvc.perform(post("/account/send/{userId}")
-                        .content(toJson(userId))
-                        .content(toJson(loginDto))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+	@DisplayName("[적금 계좌 생성 테스트]")
+	@Test
+	void savingTest() throws Exception {
+
+		// 회원가입
+		// given
+		JoinDto joinDto = new JoinDto("aaaa", "ab12", "홍길동", 1234);
+
+		// when, then
+		mockMvc.perform(post("/user/join")
+				.content(toJson(joinDto))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+
+		SavingAccountPwDto savingAccountPwDto = new SavingAccountPwDto(1000);
+
+		// when, then
+		mockMvc.perform(post("/account/create/{userId}", joinDto.userId())
+				.content(toJson(savingAccountPwDto))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
+
+	@DisplayName("[송금 테스트]")
+	@Test
+	void sendTest() throws Exception {
+
+		// given
+		Long userId = 1L;
+		SendDto loginDto = new SendDto(11111111L, 5000, 1234);
+
+		// when, then
+		mockMvc.perform(post("/account/send/{userId}")
+				.content(toJson(userId))
+				.content(toJson(loginDto))
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}
 }
