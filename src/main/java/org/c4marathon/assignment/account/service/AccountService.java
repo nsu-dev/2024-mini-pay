c69f8b38d22a72fb5e1fbf28442b7d488fcd5a4d
jwj9127 <jwj9127@naver.com>
2024-09-23 15:08:58 +0900

    [fix]: 일일 한도 기능 추가, 트랜잭션 격리 레벨 하향, 불필요한 save() 메서드 삭제, 적금계좌 생성 로직 수정, 송금시 예외처리 기능추가,

--- src/main/java/org/c4marathon/assignment/account/service/AccountService.java
+++ src/main/java/org/c4marathon/assignment/account/service/AccountService.java
@@ -1,91 +1,101 @@
 package org.c4marathon.assignment.account.service;
 
-import org.c4marathon.assignment.account.domain.Account;
-import org.c4marathon.assignment.account.domain.AccountType;
-import org.c4marathon.assignment.account.dto.ChargeDto;
-import org.c4marathon.assignment.account.dto.SendDto;
-import org.c4marathon.assignment.user.domain.User;
-import org.c4marathon.assignment.user.repository.UserRepository;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.c4marathon.assignment.account.repository.AccountRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+
+import org.c4marathon.assignment.account.domain.Account;
+import org.c4marathon.assignment.account.domain.AccountType;
+import org.c4marathon.assignment.account.dto.ChargeDto;
+import org.c4marathon.assignment.account.dto.SavingAccountPwDto;
+import org.c4marathon.assignment.account.dto.SendDto;
+import org.c4marathon.assignment.account.exception.MainAccountException;
+import org.c4marathon.assignment.account.exception.NotFountAccountException;
+import org.c4marathon.assignment.account.repository.AccountRepository;
+import org.c4marathon.assignment.common.exception.BaseException;
+import org.c4marathon.assignment.user.domain.User;
+import org.c4marathon.assignment.user.repository.UserRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
-    private final AccountRepository accountRepository;
-    private final UserRepository userRepository;
-    Long random = new Random().nextLong();
+	private final AccountRepository accountRepository;
+	private final UserRepository userRepository;
+	Long randomAccountNum = new Random().nextLong();
 
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public boolean chargeMainAccount(ChargeDto chargeDto) {
+	@Scheduled(cron = "0 0 0 * * *")
+	@Transactional(isolation = Isolation.SERIALIZABLE)
+	public void resetLimitAccount() {
+		List<Account> accounts = accountRepository.findAll();
+		for (Account account : accounts) {
+			account.resetLimitAccount();
+		}
+	}
 
-        Optional<Account> optionalAccount = accountRepository.findByAccountNum(chargeDto.accountNum());
+	@Transactional(isolation = Isolation.REPEATABLE_READ)
+	public boolean chargeMainAccount(ChargeDto chargeDto) {
 
-        if (optionalAccount.isPresent()) {
-            Account account = optionalAccount.get();
+		Optional<Account> optionalAccount = accountRepository.findByAccount(chargeDto.accountNum());
 
-            account.setAmount(chargeDto.chargeMoney());
+		if (optionalAccount.isPresent()) {
+			Account account = optionalAccount.get();
 
-            accountRepository.save(account);
-            return true;
-        }
+			account.setAmount(chargeDto.chargeMoney());
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public boolean craeteSavingAccount(Long userId){
-        Optional<User> userOptional  = userRepository.findByUserId(String.valueOf(userId));
+	@Transactional(isolation = Isolation.REPEATABLE_READ)
+	public boolean craeteSavingAccount(Long userId, SavingAccountPwDto savingAccountPwDto) {
+		Optional<User> userOptional = userRepository.findByUserId(String.valueOf(userId));
 
-        if (userOptional .isPresent()){
-            User user = userOptional.get();
-            Account account = Account.builder()
-                    .accountNum(random)
-                    .type(AccountType.SAVING_ACCOUNT)
-                    .accountPw(1234)
-                    .amount(0)
-                    .user(user)
-                    .build();
+		if (userOptional.isPresent()) {
+			User user = userOptional.get();
+			Account account = Account.builder()
+				.accountNum(randomAccountNum)
+				.type(AccountType.SAVING_ACCOUNT)
+				.accountPw(savingAccountPwDto.accountPw())
+				.amount(0)
+				.user(user)
+				.build();
 
-            accountRepository.save(account);
-            return true;
-        }else {
-            return false;
-        }
-    }
+			accountRepository.save(account);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public boolean sendSavingAccount(Long userId,SendDto sendDto){
-        Optional<Account> optionalAccount = accountRepository.findByAccountNum(sendDto.accountNum());
-        Optional<Account> optionalAccountOther = accountRepository.findByAccountPw(sendDto.accountPw());
-        Optional<Account> mainAccount = accountRepository.findByUser_IdAndType(userId, AccountType.MAIN_ACCOUNT);
+	@Transactional(isolation = Isolation.SERIALIZABLE)
+	public boolean sendSavingAccount(Long userId, SendDto sendDto) {
+		Optional<Account> optionalAccount = accountRepository.findByAccount(sendDto.accountNum());
+		Optional<Account> optionalMyAccount = accountRepository.findByMyAccount(sendDto.accountPw());
+		Optional<Account> mainAccount = accountRepository.findByUser_IdAndType(userId, AccountType.MAIN_ACCOUNT);
 
-        if (optionalAccount.isPresent() && optionalAccountOther.isPresent()){
+		optionalAccount.orElseThrow(() -> new BaseException(NotFountAccountException.NOT_FOUNT_ACCOUNT));
+		optionalMyAccount.orElseThrow(() -> new BaseException(NotFountAccountException.NOT_MATCH_ACCOUNT));
 
-            Account main = mainAccount.get();
-            Account saving = optionalAccount.get();
-            int checkMoney = main.getAmount() - sendDto.sendMoney();
+		Account main = mainAccount.get();
+		Account saving = optionalAccount.get();
+		int checkMoney = main.getAmount() - sendDto.sendMoney();
 
-            if (checkMoney > 0){
-
-                main.minusAmount(sendDto.sendMoney());
-                saving.plusAmount(sendDto.sendMoney());
-                accountRepository.save(main);
-                accountRepository.save(saving);
-                return true;
-
-            }else {
-                throw new NullPointerException();
-            }
-        }else {
-            return false;
-        }
-    }
+		if (checkMoney > 0) {
+			main.reduceAmount(sendDto.sendMoney());
+			saving.increaseAmount(sendDto.sendMoney());
+			accountRepository.save(main);
+			accountRepository.save(saving);
+			return true;
+		} else {
+			throw new BaseException(MainAccountException.SHORT_MONEY);
+		}
+	}
 }
