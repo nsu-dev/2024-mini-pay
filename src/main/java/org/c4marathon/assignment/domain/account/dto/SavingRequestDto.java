4a42ab667fc8903446f90ec21d04338503c13acb
조아빈 <whdkqls122@naver.com>
2024-09-24 21:29:34 +0900

    [fix] : 송갬액이 0이하일때 예외발생 로직 추가

--- src/main/java/org/c4marathon/assignment/domain/account/dto/SavingRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/account/dto/SavingRequestDto.java
@@ -1,3 +1,18 @@
 package org.c4marathon.assignment.domain.account.dto;
-public record SavingRequestDto(int amount) {
+
+import static org.c4marathon.assignment.domain.account.entity.AccountErrCode.*;
+
+import org.c4marathon.assignment.domain.account.exception.AccountException;
+
+import jakarta.validation.constraints.NotNull;
+
+public record SavingRequestDto(
+	@NotNull(message = "송금액은 공백일 수 없습니다.")
+	int amount
+) {
+	public SavingRequestDto{
+		if(amount <= 0){
+			throw new AccountException(ACCOUNT_INVALID_FAIL);
+		}
+	}
 }
