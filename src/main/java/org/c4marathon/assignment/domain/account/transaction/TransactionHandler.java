11a264895cb61457a7f083fa21a4dba4fa0b4adb
조아빈 <whdkqls122@naver.com>
2024-09-27 23:06:34 +0900

    [feat] : 고립단계에 따른 메서드 추가

--- src/main/java/org/c4marathon/assignment/domain/account/transaction/TransactionHandler.java
+++ src/main/java/org/c4marathon/assignment/domain/account/transaction/TransactionHandler.java
@@ -1,10 +1,18 @@
 package org.c4marathon.assignment.domain.account.transaction;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class TransactionHandler {
-	public void runInTransaction(Action action) {
+	@Transactional(isolation = Isolation.REPEATABLE_READ)
+	public void runInRepeatableTransaction(Action action) {
+		action.act();
+	}
+
+	@Transactional(isolation = Isolation.READ_COMMITTED)
+	public void runInCommittedTransaction(Action action) {
 		action.act();
 	}
 
