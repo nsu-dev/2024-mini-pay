4fea0d02a05286b71a4381bf12801a8d8160130e
조아빈 <whdkqls122@naver.com>
2024-09-29 01:02:46 +0900

    [refactor] : 컴팩트 생성자 삭제 후 valid 어노테이션 추가

--- src/main/java/org/c4marathon/assignment/domain/account/dto/RemittanceRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/account/dto/RemittanceRequestDto.java
@@ -1,23 +1,14 @@
 package org.c4marathon.assignment.domain.account.dto;
 
-import static org.c4marathon.assignment.domain.account.entity.AccountErrCode.*;
-
-import org.c4marathon.assignment.domain.account.exception.AccountException;
-
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record RemittanceRequestDto (
+public record RemittanceRequestDto(
 	@NotNull(message = "계좌번호는 공백일 수 없습니다.")
 	Long accountNum,
 	@NotNull(message = "송금액은 공백일 수 없습니다.")
+	@Min(value = 0, message = "송금액은 0원 이하가 될 수 없습니다.") @Max(value = 3_000_000, message = "금일 한도 초과 입니다.")
 	Long remittanceAmount
-){
-	public RemittanceRequestDto{
-		if(remittanceAmount > 3_000_000){
-			throw new AccountException(ACCOUNT_DALIYCHARGELIMIT_ERR);
-		}
-		if(remittanceAmount <= 0){
-			throw new AccountException(ACCOUNT_INVALID_FAIL);
-		}
-	}
+) {
 }
--- src/main/java/org/c4marathon/assignment/domain/account/dto/SavingRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/account/dto/SavingRequestDto.java
@@ -1,18 +1,11 @@
 package org.c4marathon.assignment.domain.account.dto;
 
-import static org.c4marathon.assignment.domain.account.entity.AccountErrCode.*;
-
-import org.c4marathon.assignment.domain.account.exception.AccountException;
-
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record SavingRequestDto(
 	@NotNull(message = "송금액은 공백일 수 없습니다.")
+	@Min(value = 0, message = "송금액은 0원이하일 수 없습니다.")
 	int amount
 ) {
-	public SavingRequestDto{
-		if(amount <= 0){
-			throw new AccountException(ACCOUNT_INVALID_FAIL);
-		}
-	}
 }
