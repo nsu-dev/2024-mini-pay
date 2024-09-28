6edf3e7079bcdaef4737757325fc1c2ad366745f
jwj9127 <jwj9127@naver.com>
2024-09-28 21:32:41 +0900

    [fix]: Query 수정

--- src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
+++ src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
@@ -6,16 +6,15 @@ import org.c4marathon.assignment.account.domain.Account;
 import org.c4marathon.assignment.account.domain.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	@Query("SELECT a FROM Account a WHERE a.accountNum = :accountNum")
-	Optional findByAccount(Long accountNum);
+	Optional<Account> findByAccount(Long accountNum);
 
-	@Query("SELECT a FROM Account a WHERE a.accountPw = :accountPw")
-	Optional findByMyAccount(int accountPw);
-
-	Optional<Account> findByUser_IdAndType(Long userId, AccountType type);
+	@Query("SELECT a FROM Account a WHERE a.user.userId = :userId AND a.type = :type")
+	Optional<Account> findByMainAccount(@Param("userId") String userId, @Param("type") AccountType type);
 
 }
