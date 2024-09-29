2226e5e69a9d21bcb30ea25bd3fe8c8e65e4d22a
조아빈 <whdkqls122@naver.com>
2024-09-30 01:09:28 +0900

    [refactor] : 컴팩트 생성자 삭제 후 validation 적용

--- src/main/java/org/c4marathon/assignment/domain/account/dto/RemittanceRequestDto.java
+++ src/main/java/org/c4marathon/assignment/domain/account/dto/RemittanceRequestDto.java
@@ -8,7 +8,8 @@ public record RemittanceRequestDto(
 	@NotNull(message = "계좌번호는 공백일 수 없습니다.")
 	Long accountNum,
 	@NotNull(message = "송금액은 공백일 수 없습니다.")
-	@Min(value = 0, message = "송금액은 0원 이하가 될 수 없습니다.") @Max(value = 3_000_000, message = "금일 한도 초과 입니다.")
+	@Min(value = 0, message = "송금액은 0원 이상 이어야 합니다.")
+	@Max(value = 3_000_000, message = "금일 충전한도 초과 입니다.")
 	Long remittanceAmount
 ) {
 }
