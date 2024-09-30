1fb63ab90961be864283a84eb75a05fb9275c5c0
jwj9127 <jwj9127@naver.com>
2024-09-30 13:36:01 +0900

    [refactor]: 패키지 구분을 위한 메소드 제거

--- src/main/java/org/c4marathon/assignment/user/exception/LoginException.java
+++ src/main/java/org/c4marathon/assignment/user/exception/LoginException.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum LoginException implements ErrorCode {
-	NOT_FOUND_USER("아이디가 일치하는 사용자를 찾을 수 없습니다.", "USER_002"),
 	NOT_MATCH_PASSWORD("비밀번호가 일치하지 않습니다.", "USER_003");
 
 	private final String message;
