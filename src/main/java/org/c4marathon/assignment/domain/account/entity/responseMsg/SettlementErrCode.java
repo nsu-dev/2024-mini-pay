b54538c0da9af541057ddadf2d51bf1bc31d8a30
조아빈 <whdkqls122@naver.com>
2024-09-30 23:02:56 +0900

    [feat] : 정산 에러 메시지 enum 추가

--- src/main/java/org/c4marathon/assignment/domain/account/entity/responseMsg/SettlementErrCode.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/responseMsg/SettlementErrCode.java
@@ -1,2 +1,15 @@
-package org.c4marathon.assignment.domain.account.entity.responseMsg;public class SettlementErrCode {
+package org.c4marathon.assignment.domain.account.entity.responseMsg;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public enum SettlementErrCode {
+	SETTLEMENT_FAIL(400, SettlementResponseMsg.REQUEST_FAIL.getResponseMsg()),
+	SETTLEMENT_NOT_FOUND(404, SettlementResponseMsg.SETTLEMENT_NOT_FOUND.getResponseMsg());
+
+	private final int status;
+	private final String message;
 }
