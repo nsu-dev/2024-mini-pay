c0fa57767eda835c03ad3cad973dc85a4b275bd7
jwj9127 <jwj9127@naver.com>
2024-09-30 19:35:37 +0900

    [feat]: 예외처리 추가

--- src/main/java/org/c4marathon/assignment/account/exception/NotFountAccountException.java
+++ src/main/java/org/c4marathon/assignment/account/exception/NotFountAccountException.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum NotFountAccountException implements ErrorCode {
 	NOT_FOUNT_ACCOUNT("계좌번호와 일치하는 계좌를 찾을 수 없습니다", "ACCOUNT_003"),
-	NOT_MATCH_ACCOUNT("비밀번호와 일치하는 계좌를 찾을 수 없습니다", "ACCOUNT_004");
+	NOT_MATCH_ACCOUNT("비밀번호와 일치하는 계좌를 찾을 수 없습니다", "ACCOUNT_004"),
+	NOT_FOUNT_MAIN_ACCOUNT("아이디에 일치하는 메인 계좌를 찾을 수 없습니다", "ACCOUNT_005");
 
 	private final String message;
 	private final String code;
