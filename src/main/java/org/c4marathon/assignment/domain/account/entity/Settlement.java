95e1c68b92da2a2849daea315589ca4a00754bb7
조아빈 <whdkqls122@naver.com>
2024-09-30 18:05:24 +0900

    [feat] : 정산 entity 추가

--- src/main/java/org/c4marathon/assignment/domain/account/entity/Settlement.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/Settlement.java
@@ -1,2 +1,35 @@
-package org.c4marathon.assignment.domain.account.entity;public class Settlement {
+package org.c4marathon.assignment.domain.account.entity;
+
+import static jakarta.persistence.EnumType.*;
+
+import java.util.List;
+
+import org.c4marathon.assignment.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+public class Settlement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long settlementId;
+	@Column(name = "totalAmount")
+	private Long totalAmount;        //정산 해야할 총 금액
+	@Column(name = "settlementType")
+	@Enumerated(STRING)
+	private SettlementType settleType;
+	@Column(name = "numberOfUsers")
+	private int numberOfUsers;
+	@Column(name = "settlementTarget")
+	private List<User> settlementTarget;
+	@Column(name = "settlementStatus")
+	@Enumerated(STRING)
+	private SettlementStatus settlementStatus;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user; // 정산 요청자
 }
