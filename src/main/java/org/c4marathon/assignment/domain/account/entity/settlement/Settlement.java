6bd22c55a7d42c6daeb384616ef2520ddb42c03b
조아빈 <whdkqls122@naver.com>
2024-09-30 20:11:47 +0900

    [fix] : 엔티티 어노테이션 추가

--- src/main/java/org/c4marathon/assignment/domain/account/entity/settlement/Settlement.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/settlement/Settlement.java
@@ -7,13 +7,22 @@ import java.util.List;
 import org.c4marathon.assignment.domain.user.entity.User;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "settlement")
 public class Settlement {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
