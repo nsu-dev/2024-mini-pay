fa25c20a5a8660960136b5f835106205fe472f6f
조아빈 <whdkqls122@naver.com>
2024-09-30 18:53:11 +0900

    [feat] : 정산 응답 enum 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/responseMsg/SettlementResponseMsg.java
@@ -0,0 +1,14 @@
+package org.c4marathon.assignment.domain.account.entity.responseMsg;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public enum SettlementResponseMsg {
+	REQUEST_COMPLETED("정산요청이 완료되었습니다."),
+	REQUEST_FAIL("정산요청이 실패하였습니다.");
+	
+	private final String responseMsg;
+}
