cb518782570ba16667858dd32576d9050fdd6498
조아빈 <whdkqls122@naver.com>
2024-09-30 23:20:24 +0900

    [fix] : 필드 타입 수정

--- src/main/java/org/c4marathon/assignment/domain/settlement/exception/SettlementException.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/exception/SettlementException.java
@@ -1,6 +1,6 @@
 package org.c4marathon.assignment.domain.settlement.exception;
 
-import org.c4marathon.assignment.domain.account.entity.responseMsg.AccountErrCode;
+import org.c4marathon.assignment.domain.settlement.entity.responsemsg.SettlementErrCode;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,5 +8,5 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class SettlementException extends RuntimeException {
-	private final AccountErrCode accountErrCode;
+	private final SettlementErrCode settlementErrCode;
 }
--- src/main/java/org/c4marathon/assignment/domain/settlement/service/SettlementService.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/service/SettlementService.java
@@ -1,10 +1,14 @@
-package org.c4marathon.assignment.domain.settlement;
+package org.c4marathon.assignment.domain.settlement.service;
 
+import static org.c4marathon.assignment.domain.settlement.entity.responsemsg.SettlementErrCode.*;
 import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
 
 import java.util.List;
 
-import org.c4marathon.assignment.domain.settlement.entity.SettlementUser;
+import org.c4marathon.assignment.domain.settlement.dto.response.SettlementResponseDto;
+import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementUser;
+import org.c4marathon.assignment.domain.settlement.exception.SettlementException;
+import org.c4marathon.assignment.domain.settlement.repository.SettlementUserRepository;
 import org.c4marathon.assignment.domain.user.entity.User;
 import org.c4marathon.assignment.domain.user.exception.UserException;
 import org.c4marathon.assignment.domain.user.repository.UserRepository;
@@ -23,7 +27,8 @@ public class SettlementService {
 	public List<SettlementResponseDto> findAllSettlement(HttpServletRequest httpServletRequest) {
 		Long userId = getSessionId(httpServletRequest);
 		User user = userRepository.findById(userId).orElseThrow(() -> new UserException(USER_NOT_FOUND));
-		List<SettlementUser> settlementUserList = settlementUserRepository.findAllByUser(user).orElseThrow();
+		List<SettlementUser> settlementUserList = settlementUserRepository.findAllByUser(user)
+			.orElseThrow(() -> new SettlementException(SETTLEMENT_NOT_FOUND));
 	}
 
 	private Long getSessionId(HttpServletRequest httpServletRequest) {
