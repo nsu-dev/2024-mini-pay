5acea34f8bdf9c526ce0512b5ec4c79a1174442d
조아빈 <whdkqls122@naver.com>
2024-09-30 22:47:13 +0900

    [feat] : 정산 대상유저가 정산목록 조회 시 반환될 dto 추가

--- src/main/java/org/c4marathon/assignment/domain/account/dto/response/SettlementResponseDto.java
+++ src/main/java/org/c4marathon/assignment/domain/account/dto/response/SettlementResponseDto.java
@@ -1,2 +1,14 @@
-package org.c4marathon.assignment.domain.account.dto.response;public class SettlementResponseDto {
+package org.c4marathon.assignment.domain.account.dto.response;
+
+import org.c4marathon.assignment.domain.account.entity.settlement.SettlementStatus;
+import org.c4marathon.assignment.domain.account.entity.settlement.SettlementType;
+
+public class SettlementResponseDto {
+	private Long settlementId;
+	private Long totalAmount;
+	private SettlementType settlementType;
+	private int numberOfUsers;
+	private int remainingUsers;
+	private Long remainingAmount;
+	private SettlementStatus settlementStatus;
 }
