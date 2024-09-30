f78540c3d26a21ef6fb4f1f057dcf7c646eb374e
조아빈 <whdkqls122@naver.com>
2024-09-30 22:20:31 +0900

    [feat] : SettlementRepository 추가

--- src/main/java/org/c4marathon/assignment/domain/account/repository/SettlementRepository.java
+++ src/main/java/org/c4marathon/assignment/domain/account/repository/SettlementRepository.java
@@ -1,2 +1,7 @@
-package org.c4marathon.assignment.domain.account.repository;public interface SettlementRepository {
+package org.c4marathon.assignment.domain.account.repository;
+
+import org.c4marathon.assignment.domain.account.entity.settlement.Settlement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SettlementRepository extends JpaRepository<Settlement, Long> {
 }
