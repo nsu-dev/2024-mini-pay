25436cdbb8bc8f3e1c6301d66284e36e8b2857df
조아빈 <whdkqls122@naver.com>
2024-09-30 22:59:48 +0900

    [feat] : enum 필드 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/responseMsg/SettlementErrCode.java
@@ -0,0 +1,2 @@
+package org.c4marathon.assignment.domain.account.entity.responseMsg;public class SettlementErrCode {
+}
--- src/main/java/org/c4marathon/assignment/domain/account/entity/responseMsg/SettlementResponseMsg.java
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/responseMsg/SettlementResponseMsg.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @Getter
 public enum SettlementResponseMsg {
 	REQUEST_COMPLETED("정산요청이 완료되었습니다."),
-	REQUEST_FAIL("정산요청이 실패하였습니다.");
-	
+	REQUEST_FAIL("정산요청이 실패하였습니다."),
+	SETTLEMENT_NOT_FOUND("정산 내역이 없습니다.");
 	private final String responseMsg;
 }
