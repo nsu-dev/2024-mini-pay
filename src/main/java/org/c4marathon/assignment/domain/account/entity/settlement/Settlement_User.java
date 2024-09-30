683fe5e6b53f605ebad8d390574d5b14a8851350
조아빈 <whdkqls122@naver.com>
2024-09-30 22:03:30 +0900

    [feat] : 정산과 유저의 중간 매핑테이블 추가

---
+++ src/main/java/org/c4marathon/assignment/domain/account/entity/settlement/Settlement_User.java
@@ -0,0 +1,30 @@
+package org.c4marathon.assignment.domain.account.entity.settlement;
+
+import org.c4marathon.assignment.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "settlement_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Settlement_User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long settlementUserId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Settlement settlement;
+}
