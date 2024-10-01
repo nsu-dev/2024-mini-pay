df6a472634fe12d93d7c873de232681aaab63a8e
조아빈 <whdkqls122@naver.com>
2024-10-01 18:44:03 +0900

    [feat] : 빌더 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/dto/response/SettlementResponseDto.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/dto/response/SettlementResponseDto.java
@@ -3,6 +3,8 @@ package org.c4marathon.assignment.domain.settlement.dto.response;
 import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementStatus;
 import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementType;
 
+import lombok.Builder;
+
 public class SettlementResponseDto {
 	private Long settlementId;
 	private Long totalAmount;
@@ -11,4 +13,16 @@ public class SettlementResponseDto {
 	private int remainingUsers;
 	private Long remainingAmount;
 	private SettlementStatus settlementStatus;
+
+	@Builder
+	SettlementResponseDto(Long settlementId, Long totalAmount, SettlementType settlementType, int numberOfUsers,
+		int remainingUsers, Long remainingAmount, SettlementStatus settlementStatus) {
+		this.settlementId = settlementId;
+		this.totalAmount = totalAmount;
+		this.settlementType = settlementType;
+		this.numberOfUsers = numberOfUsers;
+		this.remainingUsers = remainingUsers;
+		this.remainingAmount = remainingAmount;
+		this.settlementStatus = settlementStatus;
+	}
 }
