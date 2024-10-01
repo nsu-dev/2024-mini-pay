38b2f021360cfcdd6b240a9b0a0eb4cc256b9c7c
조아빈 <whdkqls122@naver.com>
2024-10-01 19:58:17 +0900

    [feat] : SettlementRequestDto -> Settlement 변환 매퍼 메서드 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/dto/SettlementMapper.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/dto/SettlementMapper.java
@@ -3,7 +3,10 @@ package org.c4marathon.assignment.domain.settlement.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.c4marathon.assignment.domain.settlement.dto.request.SettlementRequestDto;
 import org.c4marathon.assignment.domain.settlement.dto.response.SettlementHistoryResponseDto;
+import org.c4marathon.assignment.domain.settlement.entity.settlement.Settlement;
+import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementStatus;
 import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementUser;
 
 public class SettlementMapper {
@@ -23,5 +26,16 @@ public class SettlementMapper {
 			settlementHistoryResponseDtoList.add(settlementHistoryResponseDto);
 		}
 		return settlementHistoryResponseDtoList;
+	}
+
+	public static Settlement toSettlement(SettlementRequestDto settlementRequestDto) {
+		return Settlement.builder()
+			.totalAmount(settlementRequestDto.settlementAmount())
+			.settlementType(settlementRequestDto.settlementType())
+			.numberOfUsers(settlementRequestDto.settlementTargetList().size())
+			.remainingUsers(settlementRequestDto.settlementTargetList().size())
+			.totalAmount(settlementRequestDto.settlementAmount())
+			.settlementStatus(SettlementStatus.REQUESTED)
+			.build();
 	}
 }
