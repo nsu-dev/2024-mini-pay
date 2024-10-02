9b41eac15db468ba63b5dddc5b976d8b637f6181
jwj9127 <jwj9127@naver.com>
2024-10-02 16:18:10 +0900

    [feat]: 메서드에 비관적 LOCK 추가 및 테스트용 쿼리 메서드

--- src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
+++ src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
@@ -5,18 +5,25 @@ import java.util.Optional;
 import org.c4marathon.assignment.account.domain.Account;
 import org.c4marathon.assignment.account.domain.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	@Query("SELECT a FROM Account a WHERE a.accountNum = :accountNum")
 	Optional<Account> findByAccount(Long accountNum);
 
+	@Query("SELECT a FROM Account a WHERE a.accountNum = :accountNum AND a.type = :type")
+	Optional<Account> findByAccountNumber(@Param("accountNum") Long accountNum, @Param("type") AccountType type);
+
 	@Query("SELECT a FROM Account a WHERE a.user.userId = :userId AND a.type = :type")
 	Optional<Account> findByMainAccount(@Param("userId") String userId, @Param("type") AccountType type);
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT a FROM Account a WHERE a.accountNum = :accountNum AND a.type = :type")
 	Optional<Account> findByOtherMainAccount(@Param("accountNum") Long accountNum, @Param("type") AccountType type);
 }
