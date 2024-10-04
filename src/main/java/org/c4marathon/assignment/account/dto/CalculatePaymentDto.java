4db6cc0a4187fa2a2edeeb4f856c410b541f1d9f
jwj9127 <jwj9127@naver.com>
2024-10-04 17:54:54 +0900

    [feat]: 정산 DTO 추가

---
+++ src/main/java/org/c4marathon/assignment/account/dto/CalculatePaymentDto.java
@@ -0,0 +1,18 @@
+package org.c4marathon.assignment.account.dto;
+
+import java.util.List;
+
+import org.c4marathon.assignment.account.enums.PaymentType;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record CalculatePaymentDto(
+	@NotNull(message = "정산할 계정이 하나라도 있어야 합니다")
+	List usersId,
+	@Min(value = 0, message = "정산 금액은 0원 이상이여야 합니다.")
+	int paymentMoney,
+	@NotNull(message = "정산 타입을 하나 정해주셔야 합니다.")
+	PaymentType paymentType
+) {
+}
