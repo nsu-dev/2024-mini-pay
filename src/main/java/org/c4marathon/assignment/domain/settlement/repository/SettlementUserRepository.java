42d756895a880b149460bbeea182a459c18820be
조아빈 <whdkqls122@naver.com>
2024-10-06 19:30:18 +0900

    [refactor] : param 어노테이션 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/repository/SettlementUserRepository.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/repository/SettlementUserRepository.java
@@ -9,10 +9,14 @@ import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementU
 import org.c4marathon.assignment.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SettlementUserRepository extends JpaRepository<SettlementUser, Long> {
 	Optional<List<SettlementUser>> findAllByUser(User user);
 
 	@Query("select user from SettlementUser where settlementRole = :settlementRole and settlement = :settlement")
-	User findReceiver(SettlementRole settlementRole, Settlement settlement);
+	User findReceiver(
+		@Param("settlementRole") SettlementRole settlementRole,
+		@Param("settlement") Settlement settlement
+	);
 }
