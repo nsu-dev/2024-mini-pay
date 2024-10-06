e05703276d55e03a5a5edcc12c17a8121f220aa3
조아빈 <whdkqls122@naver.com>
2024-10-06 17:23:02 +0900

    [feat] : 정산요청 변환 및 저장 메서드 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/dto/SettlementMapper.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/dto/SettlementMapper.java
@@ -1,5 +1,8 @@
 package org.c4marathon.assignment.domain.settlement.dto;
 
+import static org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementRole.*;
+import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +11,18 @@ import org.c4marathon.assignment.domain.settlement.dto.response.SettlementHistor
 import org.c4marathon.assignment.domain.settlement.entity.settlement.Settlement;
 import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementStatus;
 import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementUser;
+import org.c4marathon.assignment.domain.settlement.repository.SettlementUserRepository;
+import org.c4marathon.assignment.domain.user.entity.User;
+import org.c4marathon.assignment.domain.user.exception.UserException;
+import org.c4marathon.assignment.domain.user.repository.UserRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class SettlementMapper {
+	private static SettlementUserRepository settlementUserRepository;
+	private static UserRepository userRepository;
+
 	public static List<SettlementHistoryResponseDto> toSettlementResponseDtoList(
 		List<SettlementUser> settlementUserList) {
 		List<SettlementHistoryResponseDto> settlementHistoryResponseDtoList = new ArrayList<>();
@@ -37,5 +50,16 @@ public class SettlementMapper {
 			.totalAmount(settlementRequestDto.settlementAmount())
 			.settlementStatus(SettlementStatus.REQUESTED)
 			.build();
+	}
+	
+	public static void toSettlementUser(SettlementRequestDto settlementRequestDto, User user, Settlement settlement) {
+		SettlementUser settlementUser = new SettlementUser(RECEIVER, user, settlement);
+		settlementUserRepository.save(settlementUser);
+		for (String senderPhone : settlementRequestDto.settlementTargetList()) {
+			User sender = userRepository.findByUserPhone(senderPhone)
+				.orElseThrow(() -> new UserException(USER_NOT_FOUND));
+			SettlementUser settlementSender = new SettlementUser(SENDER, sender, settlement);
+			settlementUserRepository.save(settlementSender);
+		}
 	}
 }
