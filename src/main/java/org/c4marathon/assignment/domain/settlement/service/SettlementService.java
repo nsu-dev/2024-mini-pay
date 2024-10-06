bad51e37f43d105e12eacd5d1fe41ca25e914fc5
조아빈 <whdkqls122@naver.com>
2024-10-06 17:21:36 +0900

    [feat] : 정산 요청 메서드 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/service/SettlementService.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/service/SettlementService.java
@@ -6,7 +6,11 @@ import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
 import java.util.List;
 
 import org.c4marathon.assignment.domain.settlement.dto.SettlementMapper;
+import org.c4marathon.assignment.domain.settlement.dto.request.SettlementRequestDto;
 import org.c4marathon.assignment.domain.settlement.dto.response.SettlementHistoryResponseDto;
+import org.c4marathon.assignment.domain.settlement.dto.response.SettlementResponseDto;
+import org.c4marathon.assignment.domain.settlement.entity.responsemsg.SettlementResponseMsg;
+import org.c4marathon.assignment.domain.settlement.entity.settlement.Settlement;
 import org.c4marathon.assignment.domain.settlement.entity.settlement.SettlementUser;
 import org.c4marathon.assignment.domain.settlement.exception.SettlementException;
 import org.c4marathon.assignment.domain.settlement.repository.SettlementRepository;
@@ -34,6 +38,18 @@ public class SettlementService {
 		List<SettlementUser> settlementUserList = settlementUserRepository.findAllByUser(user)
 			.orElseThrow(() -> new SettlementException(SETTLEMENT_NOT_FOUND));
 		return SettlementMapper.toSettlementResponseDtoList(settlementUserList);
+	}
+
+	//정산 요청 메서드
+	public SettlementResponseDto requestSettlement(
+		SettlementRequestDto settlementRequestDto,
+		HttpServletRequest httpServletRequest
+	) {
+		Settlement settlement = SettlementMapper.toSettlement(settlementRequestDto);
+		Long userId = getSessionId(httpServletRequest);
+		User user = userRepository.findById(userId).orElseThrow(() -> new UserException(USER_NOT_FOUND));
+		SettlementMapper.toSettlementUser(settlementRequestDto, user, settlement);
+		return new SettlementResponseDto(SettlementResponseMsg.REQUEST_COMPLETED.getResponseMsg());
 	}
 
 	private Long getSessionId(HttpServletRequest httpServletRequest) {
