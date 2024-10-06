85ffae23855bb1855b7ea93919602c1628252120
조아빈 <whdkqls122@naver.com>
2024-10-06 23:31:34 +0900

    [feat] : 생성자 및 정산대상 판별 컬럼 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/entity/settlement/SettlementUser.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/entity/settlement/SettlementUser.java
@@ -1,8 +1,12 @@
 package org.c4marathon.assignment.domain.settlement.entity.settlement;
 
+import static jakarta.persistence.EnumType.*;
+
 import org.c4marathon.assignment.domain.user.entity.User;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,9 +26,19 @@ public class SettlementUser {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long settlementUserId;
 
+	@Column(name = "settlementRole")
+	@Enumerated(STRING)
+	private SettlementRole settlementRole;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	private User user;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Settlement settlement;
+
+	public SettlementUser(SettlementRole settlementRole, User user, Settlement settlement) {
+		this.settlementRole = settlementRole;
+		this.user = user;
+		this.settlement = settlement;
+	}
 }
