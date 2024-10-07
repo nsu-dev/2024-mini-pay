e4eb5fce7574a8ee5155c9ecd0a28f969c4d7c71
jwj9127 <jwj9127@naver.com>
2024-10-07 22:35:20 +0900

    [fix]: List 타입 List<String>으로 변경

--- src/main/java/org/c4marathon/assignment/account/dto/CalculatePaymentDto.java
+++ src/main/java/org/c4marathon/assignment/account/dto/CalculatePaymentDto.java
@@ -9,7 +9,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record CalculatePaymentDto(
 	@NotNull(message = "정산할 계정이 하나라도 있어야 합니다")
-	List usersId,
+	List<String> usersId,
 	@Min(value = 0, message = "정산 금액은 0원 이상이여야 합니다.")
 	int paymentMoney,
 	@NotNull(message = "정산 타입을 하나 정해주셔야 합니다.")
