16b4909c387fd55aca84c6d6c2a17e4cce4b46df
조아빈 <whdkqls122@naver.com>
2024-10-07 17:17:32 +0900

    [feat] : 서버에러 반환 메시지 추가

--- src/main/java/org/c4marathon/assignment/domain/settlement/entity/responsemsg/SettlementErrCode.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/entity/responsemsg/SettlementErrCode.java
@@ -10,7 +10,8 @@ public enum SettlementErrCode {
 	SETTLEMENT_FAIL(400, SettlementResponseMsg.REQUEST_FAIL.getResponseMsg()),
 	SETTLEMENT_INVALID_TYPE(400, SettlementResponseMsg.SETTLEMENT_INVALID_TYPE.getResponseMsg()),
 	SETTLEMENT_INVALID_FAIL(400, SettlementResponseMsg.SETTLEMENT_INVALID_FAIL.getResponseMsg()),
-	SETTLEMENT_NOT_FOUND(404, SettlementResponseMsg.SETTLEMENT_NOT_FOUND.getResponseMsg());
+	SETTLEMENT_NOT_FOUND(404, SettlementResponseMsg.SETTLEMENT_NOT_FOUND.getResponseMsg()),
+	SETTLEMENT_SERVER_ERROR(500, SettlementResponseMsg.SETTLEMENT_SERVER_ERROR.getResponseMsg());
 
 	private final int status;
 	private final String message;
--- src/main/java/org/c4marathon/assignment/domain/settlement/entity/responsemsg/SettlementResponseMsg.java
+++ src/main/java/org/c4marathon/assignment/domain/settlement/entity/responsemsg/SettlementResponseMsg.java
@@ -11,6 +11,8 @@ public enum SettlementResponseMsg {
 	REQUEST_FAIL("정산요청이 실패하였습니다."),
 	SETTLEMENT_NOT_FOUND("정산 내역이 없습니다."),
 	SETTLEMENT_INVALID_TYPE("정산 유형이 잘못되었습니다."),
-	SETTLEMENT_INVALID_FAIL("공백일 수 없습니다.");
+	SETTLEMENT_INVALID_FAIL("공백일 수 없습니다."),
+	SETTLEMENT_SERVER_ERROR("서버 에러 입니다. 다시 시도 부탁드립니다.");
+	
 	private final String responseMsg;
 }
