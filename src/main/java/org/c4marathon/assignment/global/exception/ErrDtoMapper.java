e1ab05797536fe30589fb0333dee579f8675bfa0
조아빈 <whdkqls122@naver.com>
2024-10-07 16:58:24 +0900

    [feat] : ErrDtoMapper 생성 및 각 ErrDto변환메서드 오버로딩하여 추가

--- src/main/java/org/c4marathon/assignment/global/exception/ErrDtoMapper.java
+++ src/main/java/org/c4marathon/assignment/global/exception/ErrDtoMapper.java
@@ -1,4 +1,50 @@
-package org.c4marathon.assignment.global;
+package org.c4marathon.assignment.global.exception;
 
+import org.c4marathon.assignment.domain.account.dto.response.AccountErrDto;
+import org.c4marathon.assignment.domain.account.entity.responseMsg.AccountErrCode;
+import org.c4marathon.assignment.domain.user.dto.response.UserErrDto;
+import org.c4marathon.assignment.domain.user.entity.UserErrCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ErrDtoMapper {
+	public ResponseEntity<AccountErrDto> getAccountErrDto(AccountErrCode errCode) {
+		return getErrDto(errCode, errCode.getMessage());
+	}
+
+	public ResponseEntity<UserErrDto> getUserErrDto(UserErrCode errCode) {
+		return getErrDto(errCode, errCode.getMessage());
+	}
+
+	public ResponseEntity<AccountErrDto> getErrDto(AccountErrCode errCode, String errMsg) {
+		HttpStatus httpStatus;
+		try {
+			httpStatus = HttpStatus.valueOf(errCode.getStatus());
+		} catch (IllegalArgumentException e) {
+			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		AccountErrDto errDto = new AccountErrDto(
+			errCode.getStatus(),
+			errMsg != null ? errMsg : errCode.getMessage(),
+			httpStatus
+		);
+		return new ResponseEntity<>(errDto, httpStatus);
+	}
+
+	public ResponseEntity<UserErrDto> getErrDto(UserErrCode errCode, String errMsg) {
+		HttpStatus httpStatus;
+		try {
+			httpStatus = HttpStatus.valueOf(errCode.getStatus());
+		} catch (IllegalArgumentException e) {
+			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		UserErrDto errDto = new UserErrDto(
+			errCode.getStatus(),
+			errMsg != null ? errMsg : errCode.getMessage(),
+			httpStatus
+		);
+		return new ResponseEntity<>(errDto, httpStatus);
+	}
 }
