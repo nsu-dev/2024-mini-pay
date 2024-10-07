3e493f12f57fe3f5461c19a23878f1ddd6714f03
조아빈 <whdkqls122@naver.com>
2024-10-07 16:56:52 +0900

    [refactor] : AccountException처리 추가 및 errDto 변환 메서드 외부 클래스 메서드로 변경

--- src/main/java/org/c4marathon/assignment/domain/user/exception/UserExceptionHandler.java
+++ src/main/java/org/c4marathon/assignment/domain/user/exception/UserExceptionHandler.java
@@ -5,53 +5,46 @@ import static org.c4marathon.assignment.domain.user.entity.UserErrCode.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.c4marathon.assignment.domain.account.dto.response.AccountErrDto;
+import org.c4marathon.assignment.domain.account.exception.AccountException;
 import org.c4marathon.assignment.domain.user.controller.UserController;
 import org.c4marathon.assignment.domain.user.dto.response.UserErrDto;
-import org.c4marathon.assignment.domain.user.entity.UserErrCode;
-import org.springframework.http.HttpStatus;
+import org.c4marathon.assignment.global.exception.ErrDtoMapper;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.RequiredArgsConstructor;
+
 @RestControllerAdvice(basePackageClasses = UserController.class)
+@RequiredArgsConstructor
 public class UserExceptionHandler {
+	private final ErrDtoMapper errDtoMapper;
+
 	@ExceptionHandler({UserException.class})
 	protected ResponseEntity<UserErrDto> handleUserException(UserException ex) {
-		return getUserErrDto(ex.getUserErrCode());
+		return errDtoMapper.getUserErrDto(ex.getUserErrCode());
 	}
 
 	@ExceptionHandler({MethodArgumentNotValidException.class})
 	protected ResponseEntity<UserErrDto> handleUserInvalidException(MethodArgumentNotValidException ex) {
 		List<String> invalidMsgList = ex.getBindingResult().getFieldErrors().stream()
-			.map(e -> e.getDefaultMessage())
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
 			.collect(Collectors.toList());
 
 		String invalidMsg = String.join(", ", invalidMsgList);
-		return getUserErrDto(USER_INVALID_FAIL, invalidMsg);
+		return errDtoMapper.getErrDto(USER_INVALID_FAIL, invalidMsg);
+	}
+
+	@ExceptionHandler({AccountException.class})
+	protected ResponseEntity<AccountErrDto> handleAccountException(AccountException ex) {
+		return errDtoMapper.getAccountErrDto(ex.getAccountErrCode());
 	}
 
 	@ExceptionHandler({RuntimeException.class})
 	protected ResponseEntity<UserErrDto> handleUserRuntimeException() {
-		return getUserErrDto(USER_SERVER_ERROR);
-	}
-
-	private ResponseEntity<UserErrDto> getUserErrDto(UserErrCode errCode) {
-		return getUserErrDto(errCode, errCode.getMessage());
-	}
-
-	private ResponseEntity<UserErrDto> getUserErrDto(UserErrCode errCode, String errMsg) {
-		HttpStatus httpStatus;
-		try {
-			httpStatus = HttpStatus.valueOf(errCode.getStatus());
-		} catch (IllegalArgumentException e) {
-			httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-		}
-		UserErrDto errDto = new UserErrDto(
-			errCode.getStatus(),
-			errMsg != null ? errMsg : errCode.getMessage(),
-			httpStatus
-		);
-		return new ResponseEntity<>(errDto, httpStatus);
+		return errDtoMapper.getUserErrDto(USER_SERVER_ERROR);
 	}
 }
