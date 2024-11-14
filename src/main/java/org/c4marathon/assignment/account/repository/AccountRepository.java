c2e4ae6f5f4343479aef81a0573378192a175bf0
dudxo <father8475@naver.com>
2024-11-14 19:42:51 +0900

    [feat]: 비관적 락 쿼리 메서드 추가

--- src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
+++ src/main/java/org/c4marathon/assignment/account/repository/AccountRepository.java
@@ -1,11 +1,12 @@
 package org.c4marathon.assignment.account.repository;
 
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
-
 import org.c4marathon.assignment.account.domain.Account;
 import org.c4marathon.assignment.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,8 +14,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-	List<Account> findAllByUser(User user);
+    List<Account> findAllByUser(User user);
 
-	@Query("SELECT a FROM Account a JOIN FETCH a.user WHERE a.id = :id")
-	Optional<Account> findById(@Param("id") Long accountId);
+    @Query("SELECT a FROM Account a JOIN FETCH a.user WHERE a.id = :id")
+    Optional<Account> findById(@Param("id") Long accountId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a JOIN FETCH a.user WHERE a.id = :id")
+    Optional<Account> findByIdWithLock(@Param("id") Long accountId);
 }
